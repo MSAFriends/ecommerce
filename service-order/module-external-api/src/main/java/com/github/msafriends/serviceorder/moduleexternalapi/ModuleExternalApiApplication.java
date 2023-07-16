f40fd60dde5aa065cd34ef194dcb875d26fb8b1a
BINARYBARD <binarybard@pm.me>
2023-07-16 17:49:45 +0900

    chore: design a multi-module architecture for Order Service

---
+++ service-order/module-external-api/src/main/java/com/github/msafriends/serviceorder/moduleexternalapi/ModuleExternalApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.msafriends.serviceorder.moduleexternalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleExternalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleExternalApiApplication.class, args);
+    }
+
+}
