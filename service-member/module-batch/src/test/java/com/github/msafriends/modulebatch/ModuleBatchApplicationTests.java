3f05ba1f1b9f942e8e6eb53a867e409bf15819a5
In_Sehoon <49235528+oereo@users.noreply.github.com>
2023-07-16 17:46:09 +0900

    [member-service] feat: designing the multimodule structure for the member service (#2)
    undefined

---
+++ service-member/module-batch/src/test/java/com/github/msafriends/modulebatch/ModuleBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.msafriends.modulebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
