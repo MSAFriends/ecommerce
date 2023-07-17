36e653a067bc4fe73579535c29e89bae6cf5fc4c
oereo <dlstpgns0406@gmail.com>
2023-07-17 15:13:40 +0900

    feat: Notification, NotificationSenderType, NotificationType class 추가
    - Notification: 푸시 알림 Entity
    - NotificationSenderType: 푸시 알림 전송 보낸 대상자 타입
    - NotificationType: 푸시 알림 이벤트 타입

---
+++ service-member/module-core/src/main/java/com/github/msafriends/modulecore/domain/notification/NotificationType.java
@@ -0,0 +1,8 @@
+package com.github.msafriends.modulecore.domain.notification;
+
+public enum NotificationType {
+    ORDER_REJECTED_NOTIFICATION,
+    ORDER_COMPLETED_NOTIFICATION,
+    COUPON_DISTRIBUTION_NOTIFICATION,
+    MANAGER_NEW_EVENT_NOTIFICATION
+}
