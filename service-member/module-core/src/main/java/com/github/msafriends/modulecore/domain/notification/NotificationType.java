68b6d45ab43217d1f0cbc91618b09526707c0dfc
oereo <dlstpgns0406@gmail.com>
2023-07-18 17:15:26 +0900

    feat: NotificationType enum class의 생성자 및 field format 변경

--- service-member/module-core/src/main/java/com/github/msafriends/modulecore/domain/notification/NotificationType.java
+++ service-member/module-core/src/main/java/com/github/msafriends/modulecore/domain/notification/NotificationType.java
@@ -1,8 +1,19 @@
 package com.github.msafriends.modulecore.domain.notification;
 
+import lombok.Getter;
+
+@Getter
 public enum NotificationType {
-    ORDER_REJECTED_NOTIFICATION,
-    ORDER_COMPLETED_NOTIFICATION,
-    COUPON_DISTRIBUTION_NOTIFICATION,
-    MANAGER_NEW_EVENT_NOTIFICATION
+    ORDER_REJECTED("ORDER_REJECTED", "{sellerName}님이 주문을 거절하셨습니다."),
+    ORDER_COMPLETED("ORDER_COMPLETED", "{sellerName}님으로 부터 주문접수가 완료하셨습니다."),
+    COUPON_DISTRIBUTION("COUPON_DISTRIBUTION", "{couponName} 쿠폰이 발급되었습니다."),
+    MANAGER_NEW_EVENT("MANAGER_NEW_EVENT", "{managerName}님의 새로운 이벤트가 등록되었습니다.");
+
+    private final String code;
+    private final String messageTemplate;
+
+    NotificationType(String code, String messageTemplate) {
+        this.code = code;
+        this.messageTemplate = messageTemplate;
+    }
 }
