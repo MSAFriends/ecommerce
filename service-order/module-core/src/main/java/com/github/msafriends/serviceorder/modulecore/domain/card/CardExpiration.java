29a9dd6dd092c8b6a6ca8c4983b13d82016788c7
BINARYBARD <binarybard@pm.me>
2023-07-18 18:18:05 +0900

    feat: Add validations to CardExpiration

--- service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/domain/card/CardExpiration.java
+++ service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/domain/card/CardExpiration.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Embeddable;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
 
 @Getter
 @Embeddable
@@ -12,14 +15,37 @@ import lombok.NoArgsConstructor;
 public class CardExpiration {
 
     @Column(nullable = false)
-    private String year;
+    private Integer year;
 
     @Column(nullable = false)
-    private String month;
+    private Integer month;
 
     @Builder
-    public CardExpiration(String year, String month) {
+    public CardExpiration(Integer year, Integer month) {
+        validateCardExpiration(year, month);
+
         this.year = year;
         this.month = month;
+    }
+
+    private void validateCardExpiration(Integer year, Integer month) {
+        validateNotNull(year, month);
+        validateRange(year, month);
+    }
+
+    private void validateNotNull(Integer year, Integer month) {
+        Assert.notNull(year, "year must not be null");
+        Assert.notNull(month, "month must not be null");
+    }
+
+    private void validateRange(Integer year, Integer month) {
+        Assert.isTrue(year >= 0, "year must be greater than or equal to 0");
+        Assert.isTrue(month >= 1 && month <= 12, "month must be between 1 and 12");
+    }
+
+    public boolean isExpired() {
+        LocalDate expiryDate = LocalDate.of(this.year, this.month, 1);
+        LocalDate currentDate = LocalDate.now();
+        return currentDate.isAfter(expiryDate);
     }
 }
