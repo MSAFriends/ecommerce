2b8174bb9aa284a6bdac2d2d33bfa08ac72741ad
BINARYBARD <binarybard@pm.me>
2023-07-18 18:07:54 +0900

    feat: Add validations to CardInformation

--- service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/domain/card/CardInformation.java
+++ service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/domain/card/CardInformation.java
@@ -7,6 +7,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 @Getter
 @Embeddable
@@ -24,8 +25,30 @@ public class CardInformation {
 
     @Builder
     public CardInformation(String cardNumber, CardExpiration expiration, String cvv) {
+        validateCardInformation(cardNumber, expiration, cvv);
+
         this.cardNumber = cardNumber;
         this.expiration = expiration;
         this.cvv = cvv;
+    }
+
+    private void validateCardInformation(String cardNumber, CardExpiration expiration, String cvv) {
+        validateNotNull(cardNumber, expiration, cvv);
+        validateCardNumber(cardNumber);
+        validateCvv(cvv);
+    }
+
+    private void validateNotNull(String cardNumber, CardExpiration expiration, String cvv) {
+        Assert.notNull(cardNumber, "cardNumber must not be null");
+        Assert.notNull(expiration, "expiration must not be null");
+        Assert.notNull(cvv, "cvv must not be null");
+    }
+
+    private void validateCardNumber(String cardNumber) {
+        Assert.isTrue(cardNumber.length() == 16, "cardNumber must be 16 digits");
+    }
+
+    private void validateCvv(String cvv) {
+        Assert.isTrue(cvv.length() == 3, "cvv must be 3 digits");
     }
 }
