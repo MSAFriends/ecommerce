51dd94b0e0ca1bfbd60549cfdb4d28b3ba784bc2
BINARYBARD <binarybard@pm.me>
2023-07-18 18:08:11 +0900

    feat: Add validations to CardPayment

--- service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/domain/card/CardPayment.java
+++ service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/domain/card/CardPayment.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 @Entity
 @Getter
@@ -32,9 +33,27 @@ public class CardPayment {
 
     @Builder
     public CardPayment(Long memberId, CardInformation cardInformation, BankProvider bankProvider, String passwordPartial) {
+        validateCardPayment(memberId, cardInformation, bankProvider, passwordPartial);
+
         this.memberId = memberId;
         this.cardInformation = cardInformation;
         this.bankProvider = bankProvider;
         this.passwordPartial = passwordPartial;
+    }
+
+    private void validateCardPayment(Long memberId, CardInformation cardInformation, BankProvider bankProvider, String passwordPartial) {
+        validateNotNull(memberId, cardInformation, bankProvider, passwordPartial);
+        validatePasswordPartial(passwordPartial);
+    }
+
+    private void validatePasswordPartial(String passwordPartial) {
+        Assert.isTrue(passwordPartial.length() == 2, "passwordPartial must be two digits");
+    }
+
+    private void validateNotNull(Long memberId, CardInformation cardInformation, BankProvider bankProvider, String passwordPartial) {
+        Assert.notNull(memberId, "memberId must not be null");
+        Assert.notNull(cardInformation, "cardInformation must not be null");
+        Assert.notNull(bankProvider, "bankProvider must not be null");
+        Assert.notNull(passwordPartial, "passwordPartial must not be null");
     }
 }
