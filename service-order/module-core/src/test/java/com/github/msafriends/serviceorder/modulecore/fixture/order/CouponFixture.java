3ff39b5192378477b0df21785747ce0e6db5558f
BINARYBARD <binarybard@pm.me>
2023-07-19 17:26:14 +0900

    feat: Add Coupon entity

--- service-order/module-core/src/test/java/com/github/msafriends/serviceorder/modulecore/fixture/order/CouponFixture.java
+++ service-order/module-core/src/test/java/com/github/msafriends/serviceorder/modulecore/fixture/order/CouponFixture.java
@@ -1,31 +1,25 @@
 package com.github.msafriends.serviceorder.modulecore.fixture.order;
 
+import com.github.msafriends.serviceorder.modulecore.domain.coupon.Coupon;
 import com.github.msafriends.serviceorder.modulecore.domain.coupon.CouponDiscountType;
-import com.github.msafriends.serviceorder.modulecore.dto.CouponResponse;
+import com.github.msafriends.serviceorder.modulecore.domain.order.Order;
 
 public class CouponFixture {
-    private static final Long DEFAULT_COUPON_ID = 1L;
-    private static final int DEFAULT_DISCOUNT_AMOUNT = 1000;
-    private static final int DEFAULT_DISCOUNT_PERCENT = 10;
-    private static final String DEFAULT_COUPON_UUID = "1234123412341234";
+    public static final int DEFAULT_DISCOUNT_AMOUNT = 1000;
 
-    public static CouponResponse createCouponWithFixedDiscount(int discountAmount) {
-        return CouponResponse.builder()
-                .value(DEFAULT_DISCOUNT_AMOUNT)
-                .hasUsed(false)
-                .uuid(DEFAULT_COUPON_UUID)
+    public static Coupon createCouponWithFixedDiscount(Order order, int discountAmount) {
+        return Coupon.builder()
+                .order(order)
+                .discountValue(discountAmount)
                 .discountType(CouponDiscountType.FIXED)
-                .value(discountAmount)
                 .build();
     }
 
-    public static CouponResponse createCouponWithPercentDiscount(int discountPercent) {
-        return CouponResponse.builder()
-                .value(DEFAULT_DISCOUNT_PERCENT)
-                .hasUsed(false)
-                .uuid(DEFAULT_COUPON_UUID)
+    public static Coupon createCouponWithPercentDiscount(Order order, int discountPercent) {
+        return Coupon.builder()
+                .order(order)
+                .discountValue(discountPercent)
                 .discountType(CouponDiscountType.PERCENTAGE)
-                .value(discountPercent)
                 .build();
     }
 }
