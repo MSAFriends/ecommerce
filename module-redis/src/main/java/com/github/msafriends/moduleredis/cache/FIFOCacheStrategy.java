28fa0888eeb5029894dadda5b7e0d7812bdfd03f
oereo <dlstpgns0406@gmail.com>
2023-07-21 16:09:39 +0900

    feat: generic type 표기 방식 수정
    - KEY -> K
    - VALUE -> V

--- module-redis/src/main/java/com/github/msafriends/moduleredis/cache/FIFOCacheStrategy.java
+++ module-redis/src/main/java/com/github/msafriends/moduleredis/cache/FIFOCacheStrategy.java
@@ -4,11 +4,11 @@ package com.github.msafriends.moduleredis.cache;
  * Interface representing First In First Out (FIFO) caching strategy.
  * Extends EvictionCacheStrategy and allows defining methods specific to FIFO strategy.
  */
-public interface FIFOCacheStrategy<KEY, VALUE> extends EvictionCacheStrategy<KEY, VALUE>{
+public interface FIFOCacheStrategy<K, V> extends EvictionCacheStrategy<K, V>{
     /**
      * Evicts the oldest item from the cache based on FIFO strategy.
      *
      * @return The evicted item from the cache, or null if no items were evicted.
      */
-    VALUE evictOldestItem();
+    V evictOldestItem();
 }
