cf3d93c468f70900d5feac7225691f9ceeb7a87b
oereo <dlstpgns0406@gmail.com>
2023-07-21 16:56:48 +0900

    docs: ReadThroughDataPersistenceStrategy interface description 재정의

--- module-redis/src/main/java/com/github/msafriends/moduleredis/cache/persistence/ReadThroughDataPersistenceStrategy.java
+++ module-redis/src/main/java/com/github/msafriends/moduleredis/cache/persistence/ReadThroughDataPersistenceStrategy.java
@@ -1,9 +1,10 @@
 package com.github.msafriends.moduleredis.cache.persistence;
 
 /**
- * ReadThroughDataPersistenceStrategy is a data persistence strategy that retrieves the value with the specified key from the cache.
- * If the value is not found in the cache, it reads the value from the underlying data source and stores it in the cache.
- */
+ * ReadThroughDataPersistenceStrategy is a data persistence strategy that allows retrieving the value
+ * with the specified key from the cache or from other underlying data sources if not found in the cache.
+ * This interface provides the flexibility to cache data using a specific key or access data from
+ * different database tables or data structures as needed. */
 public interface ReadThroughDataPersistenceStrategy<K, V> extends DataPersistenceStrategy<K, V> {
 
     /**
