86d44de9b41ee6abd5509ca68e37ec6fa927818f
oereo <dlstpgns0406@gmail.com>
2023-07-22 14:58:48 +0900

    refactor: cache -> strategy package 안으로 CacheStrategy interface 이동

--- module-redis/src/main/java/com/github/msafriends/moduleredis/cache/strategy/CacheStrategy.java
+++ module-redis/src/main/java/com/github/msafriends/moduleredis/cache/strategy/CacheStrategy.java
@@ -1,4 +1,4 @@
-package com.github.msafriends.moduleredis.cache;
+package com.github.msafriends.moduleredis.cache.strategy;
 
 import org.springframework.stereotype.Indexed;
 
--- module-redis/src/main/java/com/github/msafriends/moduleredis/cache/strategy/LRUCacheStrategy.java
+++ module-redis/src/main/java/com/github/msafriends/moduleredis/cache/strategy/LRUCacheStrategy.java
@@ -1,4 +1,4 @@
-package com.github.msafriends.moduleredis.cache;
+package com.github.msafriends.moduleredis.cache.strategy;
 
 /**
  * Interface representing Least Recently Used (LRU) caching strategy.
