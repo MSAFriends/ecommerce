4a944fc2bb3ed61e51bad79f5ac347f0e605d2ab
oereo <dlstpgns0406@gmail.com>
2023-07-23 05:35:22 +0900

    refactor: 등급 혜택 타입에서 배달비 할인 으로 수정

--- service-member/module-core/src/main/java/com/github/msafriends/modulecore/domain/grade/BenefitType.java
+++ service-member/module-core/src/main/java/com/github/msafriends/modulecore/domain/grade/BenefitType.java
@@ -3,5 +3,5 @@ package com.github.msafriends.modulecore.domain.grade;
 public enum BenefitType {
     FIXED_DISCOUNT_COUPON,
     PERCENTAGE_DISCOUNT_COUPON,
-    DELIVERY_FREE
+    DELIVERY_DISCOUNT
 }
