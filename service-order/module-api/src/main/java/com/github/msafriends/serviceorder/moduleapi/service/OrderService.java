49490e21b8a25599855bcf5ebd4e5f812ab100cd
BINARYBARD <binarybard@pm.me>
2023-07-23 22:45:22 +0900

    refactor: 정적 팩토리 메서드를 통해 객체를 변환하도록 수정함

--- service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/service/OrderService.java
+++ service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/service/OrderService.java
@@ -39,13 +39,9 @@ public class OrderService {
 
     @Transactional
     public Long createOrder(Long memberId, OrderRequest orderRequest) {
-        // 1. 가격을 알아내기 위해서 상품 ID마다 상품 서비스로 상품 정보를 요청한다.
-        // 1-1. 해당 상품이 존재하는가?
-        // 1-2. 재고가 충분한가?
         List<Product> products = getProducts(orderRequest);
         validateIfProductStockIsEnough(products);
 
-        // 2. 주문을 생성한다.
         Order order = Order.builder()
                 .memberId(memberId)
                 .status(OrderStatus.APPROVED)
@@ -54,18 +50,10 @@ public class OrderService {
                 .recipient(RecipientRequest.toRecipient(orderRequest.getRecipient()))
                 .build();
 
-        // 3. 멤버 서비스로부터 사용자가 사용할 수 있는 쿠폰 목록을 받아오고 검증한다.
-        // 3-1. 쿠폰이 존재하는가?
-        // 3-2. 쿠폰이 사용 가능한가? --> 항상 사용 가능한 쿠폰이 들어온다고 가정함
-        List<OrderCoupon> availableCoupons = memberServiceClient.getAvailableCouponsByMemberId(memberId).stream()
-                .map(couponResponse -> OrderCouponResponse.toCoupon(order, couponResponse))
-                .toList();
+        List<OrderCoupon> availableCoupons = OrderCouponResponse.toCoupons(order, memberServiceClient.getAvailableCouponsByMemberId(memberId));
         validateIfCouponExists(orderRequest, availableCoupons);
-
-        // 4. 주문 쿠폰을 생성한다.
         order.addCoupons(availableCoupons);
 
-        // 5. 재고를 차감한다.
         return orderRepository.save(order).getId();
     }
 
