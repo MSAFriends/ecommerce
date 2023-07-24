88d141ea6db57d5f48b7665c58e4f96eca5a972a
oereo <dlstpgns0406@gmail.com>
2023-07-24 17:29:17 +0900

    chore: module-external-api, module-internal-api module을 module-api로 통합

--- service-member/module-api/src/test/java/com/github/msafriends/moduleapi/ModuleApiApplicationTests.java
+++ service-member/module-api/src/test/java/com/github/msafriends/moduleapi/ModuleApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.github.msafriends.moduleinternalapi;
+package com.github.msafriends.moduleapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ModuleInternalApiApplicationTests {
+class ModuleApiApplicationTests {
 
     @Test
     void contextLoads() {
