3f1f6be46b4b3609b12792a7a2598e43bbdcc580
oereo <dlstpgns0406@gmail.com>
2023-07-25 01:48:20 +0900

    feat: MemberSignupRequest request dto class 추가 및 validate annotation 적용

---
+++ service-member/module-api/src/main/java/com/github/msafriends/dto/request/member/MemberSignupRequest.java
@@ -0,0 +1,53 @@
+package com.github.msafriends.dto.request.member;
+
+import com.github.msafriends.modulecore.domain.member.Member;
+import com.github.msafriends.validator.anntations.ValidMemberName;
+import com.github.msafriends.validator.anntations.ValidPassword;
+import com.github.msafriends.validator.anntations.ValidPhoneNumber;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberSignupRequest {
+
+    @NotBlank(message = "Email is a required field")
+    @Email(message = "The format of the email is not valid")
+    private String email;
+
+    @ValidPassword
+    private String password;
+
+    @ValidPhoneNumber
+    private String phoneNumber;
+    @ValidMemberName
+    private String name;
+
+    public MemberSignupRequest(final String email, final String password, final String phoneNumber, final String name) {
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .email(email)
+                .password(password)
+                .phoneNumber(phoneNumber)
+                .name(name)
+                .build();
+    }
+
+    @Override
+    public String toString() {
+        return "MemberSignupRequest{" +
+                "email='" + email + "'" +
+                "password='" + password + "'" +
+                "phoneNumber='" + phoneNumber + "'" +
+                "name='" + name + "}";
+    }
+}
