262edd0caa50704366d2f885b8fb416647bbe478
BINARYBARD <binarybard@pm.me>
2023-07-25 18:18:10 +0900

    fix: 지금은 필요 없는 주문 상태를 일시적으로 제거함

--- service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/domain/order/OrderStatus.java
+++ service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/domain/order/OrderStatus.java
@@ -1,8 +1,7 @@
 package com.github.msafriends.serviceorder.modulecore.domain.order;
 
 public enum OrderStatus {
-    APPROVAL_PENDING,
-    CREATE_PENDING,
+    PENDING,
     AWAITING_ACCEPTANCE,
     APPROVED,
     SELLER_REJECTED,
