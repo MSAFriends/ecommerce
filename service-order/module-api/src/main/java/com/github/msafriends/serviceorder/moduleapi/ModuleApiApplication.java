a372374877e5d9c8c58925fc1938bf1d92236b8d
BINARYBARD <binarybard@pm.me>
2023-07-26 00:14:24 +0900

    chore: 주문 서비스를 디스커버리 클라이언트로 등록함

--- service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/ModuleApiApplication.java
+++ service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/ModuleApiApplication.java
@@ -3,9 +3,9 @@ package com.github.msafriends.serviceorder.moduleapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan(basePackages = "com.github.msafriends.serviceorder.modulecore.domain")
 public class ModuleApiApplication {
