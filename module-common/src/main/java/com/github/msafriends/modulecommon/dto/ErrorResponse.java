7bc42f23075843e1e80b1cccceb7298777a7e7c2
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-26 00:33:28 +0900

    feat : add RestApi ExceptionHandler and DTO to handle Exceptions properly on its own situations and notify proper message to micro services

---
+++ module-common/src/main/java/com/github/msafriends/modulecommon/dto/ErrorResponse.java
@@ -0,0 +1,30 @@
+package com.github.msafriends.modulecommon.dto;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.github.msafriends.modulecommon.exception.ErrorCode;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+	private String message;
+	private String code;
+	@JsonIgnore
+	private HttpStatus status;
+
+	private ErrorResponse(ErrorCode errorCode) {
+		this.status = errorCode.getStatus();
+		this.code = errorCode.getCode();
+		this.message = errorCode.getMessage();
+	}
+
+	public static ErrorResponse of(ErrorCode errorCode){
+		return new ErrorResponse(errorCode);
+	}
+
+}
---
+++ module-common/src/main/java/com/github/msafriends/modulecommon/exception/RestApiExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.github.msafriends.modulecommon.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import com.github.msafriends.modulecommon.dto.ErrorResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class RestApiExceptionHandler {
+
+	@ExceptionHandler(AuthenticationException.class)
+	public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException ex){
+		ErrorResponse response = ErrorResponse.of(ErrorCode.AUTHENTICATION_FAILED);
+		return new ResponseEntity<>(response, response.getStatus());
+	}
+
+
+	@ExceptionHandler(AuthorizationException.class)
+	public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException ex){
+		ErrorResponse response = ErrorResponse.of(ErrorCode.AUTHORIZATION_FAILED);
+		return new ResponseEntity<>(response, response.getStatus());
+	}
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex){
+		ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
+		return new ResponseEntity<>(response, response.getStatus());
+	}
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+		ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
+		return new ResponseEntity<>(response, response.getStatus());
+	}
+	@ExceptionHandler(BusinessException.class)
+	public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex){
+		ErrorResponse response = ErrorResponse.of(ex.getErrorCode());
+		log.error("unexpected error has occurred");
+		return new ResponseEntity<>(response, response.getStatus());
+	}
+
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleExceptionHandler(Exception ex){
+		ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+		log.error(ex.getMessage(), ex);
+		return new ResponseEntity<>(response, response.getStatus());
+	}
+}
