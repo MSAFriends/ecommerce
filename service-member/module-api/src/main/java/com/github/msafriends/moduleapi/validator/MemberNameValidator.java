07e2bdf70557dd3ae086e2fa3cc6eb4161690f18
oereo <dlstpgns0406@gmail.com>
2023-07-25 16:58:33 +0900

    chore: package 이동으로 인한 import package url 수정

--- service-member/module-api/src/main/java/com/github/msafriends/moduleapi/validator/MemberNameValidator.java
+++ service-member/module-api/src/main/java/com/github/msafriends/moduleapi/validator/MemberNameValidator.java
@@ -1,6 +1,6 @@
-package com.github.msafriends.validator;
+package com.github.msafriends.moduleapi.validator;
 
-import com.github.msafriends.validator.anntations.ValidMemberName;
+import com.github.msafriends.moduleapi.validator.anntations.ValidMemberName;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
