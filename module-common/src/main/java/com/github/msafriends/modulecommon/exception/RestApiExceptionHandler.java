bc4d876e37d166b8c7a53bc39a3e7c6763c07d0c
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-26 10:07:00 +0900

    refactor: add logs with debug level to ArgumentValidation and AuthValidation exception handlers

--- module-common/src/main/java/com/github/msafriends/modulecommon/exception/RestApiExceptionHandler.java
+++ module-common/src/main/java/com/github/msafriends/modulecommon/exception/RestApiExceptionHandler.java
@@ -13,39 +13,43 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestControllerAdvice
 public class RestApiExceptionHandler {
-
 	@ExceptionHandler(AuthenticationException.class)
-	public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException ex){
+	public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException ex) {
 		ErrorResponse response = ErrorResponse.of(ErrorCode.AUTHENTICATION_FAILED);
+		log.debug("Authentication failed : {}", ex.getDetail());
 		return new ResponseEntity<>(response, response.getStatus());
 	}
-
 
 	@ExceptionHandler(AuthorizationException.class)
-	public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException ex){
+	public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException ex) {
 		ErrorResponse response = ErrorResponse.of(ErrorCode.AUTHORIZATION_FAILED);
-		return new ResponseEntity<>(response, response.getStatus());
-	}
-	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
-	public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex){
-		ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
-		return new ResponseEntity<>(response, response.getStatus());
-	}
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
-		ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
-		return new ResponseEntity<>(response, response.getStatus());
-	}
-	@ExceptionHandler(BusinessException.class)
-	public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex){
-		ErrorResponse response = ErrorResponse.of(ex.getErrorCode());
-		log.error("unexpected error has occurred");
+		log.debug("Request does not have authorization : {}", ex.getDetail());
 		return new ResponseEntity<>(response, response.getStatus());
 	}
 
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+		ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
+		log.debug("Parameter type is invalid");
+		return new ResponseEntity<>(response, response.getStatus());
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+		ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
+		log.debug("Argument validation has failed: {}", ex.getMessage());
+		return new ResponseEntity<>(response, response.getStatus());
+	}
+
+	@ExceptionHandler(BusinessException.class)
+	public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex) {
+		ErrorResponse response = ErrorResponse.of(ex.getErrorCode());
+		log.error("Unexpected error has occurred", ex.getMessage(), ex);
+		return new ResponseEntity<>(response, response.getStatus());
+	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorResponse> handleExceptionHandler(Exception ex){
+	public ResponseEntity<ErrorResponse> handleExceptionHandler(Exception ex) {
 		ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
 		log.error(ex.getMessage(), ex);
 		return new ResponseEntity<>(response, response.getStatus());
