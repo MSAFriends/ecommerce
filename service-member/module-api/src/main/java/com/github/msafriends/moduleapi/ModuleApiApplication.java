b8e77d68bf2775fb719aca0c32ef201220c7d40f
oereo <dlstpgns0406@gmail.com>
2023-07-27 01:41:44 +0900

    feat: EntityScan, EnableJpaRepositories annotation 추가

--- service-member/module-api/src/main/java/com/github/msafriends/moduleapi/ModuleApiApplication.java
+++ service-member/module-api/src/main/java/com/github/msafriends/moduleapi/ModuleApiApplication.java
@@ -2,8 +2,12 @@ package com.github.msafriends.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.github.msafriends"})
+@EntityScan("com.github.msafriends.modulecore")
+@EnableJpaRepositories("com.github.msafriends.modulecore")
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
