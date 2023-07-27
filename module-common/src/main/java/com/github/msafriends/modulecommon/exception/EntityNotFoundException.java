11db503484dcf1562c08529ad1136f329ff91e40
BINARYBARD <binarybard@pm.me>
2023-07-27 11:26:56 +0900

    chore: 유효하지 않은 상태를 나타내는 새로운 예외 추가

--- module-common/src/main/java/com/github/msafriends/modulecommon/exception/EntityNotFoundException.java
+++ module-common/src/main/java/com/github/msafriends/modulecommon/exception/EntityNotFoundException.java
@@ -1,11 +1,11 @@
 package com.github.msafriends.modulecommon.exception;
 
 public class EntityNotFoundException extends BusinessException {
-	public EntityNotFoundException(ErrorCode errorCode, String detail) {
-		super(errorCode, detail);
-	}
-
 	public EntityNotFoundException(ErrorCode errorCode) {
 		super(errorCode);
+	}
+
+	public EntityNotFoundException(ErrorCode errorCode, String detail) {
+		super(errorCode, detail);
 	}
 }
