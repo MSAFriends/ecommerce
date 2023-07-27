9e8b7db94dc0db90d08f2e155864c94563dc9185
BINARYBARD <binarybard@pm.me>
2023-07-27 11:26:56 +0900

    chore: 유효하지 않은 상태를 나타내는 새로운 예외 추가

--- module-common/src/main/java/com/github/msafriends/modulecommon/exception/AuthenticationException.java
+++ module-common/src/main/java/com/github/msafriends/modulecommon/exception/AuthenticationException.java
@@ -1,11 +1,11 @@
 package com.github.msafriends.modulecommon.exception;
 
 public class AuthenticationException extends BusinessException {
-	public AuthenticationException(ErrorCode errorCode, String detail) {
-		super(errorCode, detail);
-	}
-
 	public AuthenticationException(ErrorCode errorCode) {
 		super(errorCode);
+	}
+
+	public AuthenticationException(ErrorCode errorCode, String detail) {
+		super(errorCode, detail);
 	}
 }
--- module-common/src/main/java/com/github/msafriends/modulecommon/exception/InvalidValueException.java
+++ module-common/src/main/java/com/github/msafriends/modulecommon/exception/InvalidValueException.java
@@ -1,11 +1,11 @@
 package com.github.msafriends.modulecommon.exception;
 
-public class InvalidValueException extends BusinessException{
-	public InvalidValueException(ErrorCode errorCode, String detail) {
-		super(errorCode, detail);
-	}
-
+public class InvalidValueException extends BusinessException {
 	public InvalidValueException(ErrorCode errorCode) {
 		super(errorCode);
+	}
+
+	public InvalidValueException(ErrorCode errorCode, String detail) {
+		super(errorCode, detail);
 	}
 }
