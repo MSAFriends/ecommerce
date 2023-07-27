f4b29450455997f52fa411f620815757711c1e5b
BINARYBARD <binarybard@pm.me>
2023-07-27 15:46:00 +0900

    test: Order 도메인의 주문 항목 수량 변경 테스트 추가

--- service-order/module-core/src/testFixtures/java/com/github/msafriends/serviceorder/modulecore/fixture/OrderFixture.java
+++ service-order/module-core/src/testFixtures/java/com/github/msafriends/serviceorder/modulecore/fixture/OrderFixture.java
@@ -3,8 +3,7 @@ package com.github.msafriends.serviceorder.modulecore.fixture;
 import com.github.msafriends.serviceorder.modulecore.domain.order.Order;
 import com.github.msafriends.serviceorder.modulecore.domain.order.OrderStatus;
 import com.github.msafriends.serviceorder.modulecore.domain.order.Recipient;
-import com.github.msafriends.serviceorder.modulecore.dto.request.order.CartItemRequest;
-import com.github.msafriends.serviceorder.modulecore.dto.request.order.OrderRequest;
+import com.github.msafriends.serviceorder.modulecore.dto.request.order.ConfirmOrderRequest;
 import com.github.msafriends.serviceorder.modulecore.dto.request.order.RecipientRequest;
 import com.github.msafriends.serviceorder.modulecore.dto.response.order.OrderResponse;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -17,22 +16,18 @@ public class OrderFixture {
     private static final Recipient DEFAULT_RECIPIENT = RecipientFixture.createDefaultRecipient();
     private static final OrderStatus DEFAULT_STATUS = OrderStatus.PENDING;
 
-    private static Order.OrderBuilder createDefaultOrderBuilder() {
-        return Order.builder()
-                .memberId(DEFAULT_MEMBER_ID)
-                .request(DEFAULT_REQUEST)
-                .recipient(DEFAULT_RECIPIENT)
-                .products(List.of(ProductFixture.createDefaultProduct()))
-                .status(DEFAULT_STATUS);
+    private static Order.PendingOrderBuilder createDefaultPendingOrderBuilder() {
+        return Order.createPendingOrderBuilder()
+                .memberId(DEFAULT_MEMBER_ID);
     }
 
-    public static Order createDefaultOrder() {
-        return createDefaultOrderBuilder()
+    public static Order createDefaultPendingOrder() {
+        return createDefaultPendingOrderBuilder()
                 .build();
     }
 
-    public static Order createDefaultOrderWithId(Long id) {
-        Order order = createDefaultOrder();
+    public static Order createPendingOrderWithId(Long id) {
+        Order order = createDefaultPendingOrder();
         ReflectionTestUtils.setField(order, "id", id);
         return order;
     }
@@ -41,34 +36,27 @@ public class OrderFixture {
         return OrderResponse.from(order);
     }
 
-    private static OrderRequest.OrderRequestBuilder createDefaultOrderRequest() {
-        return OrderRequest.builder()
+    private static ConfirmOrderRequest.ConfirmOrderRequestBuilder createDefaultConfirmOrderRequest() {
+        return ConfirmOrderRequest.builder()
                 .request(DEFAULT_REQUEST)
-                .cartItems(List.of(CartItemFixture.createCartItemRequestWithQuantity(ProductFixture.DEFAULT_QUANTITY)))
                 .recipient(RecipientFixture.createDefaultRecipientRequest())
                 .orderCouponIds(List.of(OrderCouponFixture.DEFAULT_COUPON_ID));
     }
 
-    public static OrderRequest createOrderRequestWithCartItems(List<CartItemRequest> cartItemRequests) {
-        return createDefaultOrderRequest()
-                .cartItems(cartItemRequests)
-                .build();
-    }
-
-    public static OrderRequest createOrderRequestWithOrderCouponIds(List<Long> orderCouponIds) {
-        return createDefaultOrderRequest()
+    public static ConfirmOrderRequest createConfirmOrderRequestWithOrderCouponIds(List<Long> orderCouponIds) {
+        return createDefaultConfirmOrderRequest()
                 .orderCouponIds(orderCouponIds)
                 .build();
     }
 
-    public static OrderRequest createOrderRequestWithRecipient(RecipientRequest recipient) {
-        return createDefaultOrderRequest()
+    public static ConfirmOrderRequest createConfirmOrderRequestWithRecipient(RecipientRequest recipient) {
+        return createDefaultConfirmOrderRequest()
                 .recipient(recipient)
                 .build();
     }
 
-    public static OrderRequest createOrderRequestWithRequest(String request) {
-        return createDefaultOrderRequest()
+    public static ConfirmOrderRequest createConfirmOrderRequestWithRequest(String request) {
+        return createDefaultConfirmOrderRequest()
                 .request(request)
                 .build();
     }
