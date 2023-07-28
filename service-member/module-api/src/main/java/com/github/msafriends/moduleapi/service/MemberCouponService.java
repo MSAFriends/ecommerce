53643c05ec05c5590297647e1027ecc9b517d4d8
oereo <dlstpgns0406@gmail.com>
2023-07-29 00:38:14 +0900

    feat: memberCouponIds에 따라 쿠폰 사용 Internal API 추가
    - memberCouponIds에서 현재 멤버가 사용할 수 있는 쿠폰 id가 있을 경우 검증
    - 이미 사용한 쿠폰의 id가 있을 경우 검증

--- service-member/module-api/src/main/java/com/github/msafriends/moduleapi/service/MemberCouponService.java
+++ service-member/module-api/src/main/java/com/github/msafriends/moduleapi/service/MemberCouponService.java
@@ -2,6 +2,7 @@ package com.github.msafriends.moduleapi.service;
 
 import com.github.msafriends.moduleapi.dto.request.member.MemberCouponCommand;
 import com.github.msafriends.moduleapi.dto.request.member.MemberCouponRequest;
+import com.github.msafriends.moduleapi.dto.request.member.MemberCouponUseRequest;
 import com.github.msafriends.moduleapi.dto.response.ListResponse;
 import com.github.msafriends.moduleapi.dto.response.coupon.MemberCouponResponse;
 import com.github.msafriends.modulecore.domain.coupon.Coupon;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,12 +25,44 @@ public class MemberCouponService {
     private final MemberRepository memberRepository;
     private final CouponRepository couponRepository;
 
-    public ListResponse<MemberCouponResponse> getMemberCoupons(Long memberId) {
+    public ListResponse<MemberCouponResponse> getMemberCoupons(Long memberId, LocalDateTime currentTime) {
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new RuntimeException("Member not exist."));
 
         List<MemberCoupon> memberCoupons = memberCouponRepository.findAllByMemberAndHasUsed(member, false);
+        List<MemberCouponResponse> memberCouponResponses = memberCoupons.stream()
+                .filter(memberCoupon -> memberCoupon.hasValidRangeCouponUse(currentTime))
+                .map(MemberCouponResponse::from).toList();
+
+        return new ListResponse<>(
+                memberCouponResponses.size(),
+                memberCouponResponses
+        );
+    }
+
+    public ListResponse<MemberCouponResponse> useMemberCoupons(Long memberId, MemberCouponUseRequest request, LocalDateTime currentTime) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new RuntimeException("Member not exist."));
+        List<MemberCoupon> memberCoupons = member.getMemberCoupons().stream()
+                .filter(memberCoupon -> request.getMemberCouponIds().contains(memberCoupon.getId()))
+                .filter(memberCoupon -> memberCoupon.hasValidRangeCouponUse(currentTime))
+                .peek(memberCoupon -> memberCoupon.use(currentTime))
+                .toList();
+
+        validateRequestedCouponsExist(request, memberCoupons);
+
+        memberCouponRepository.saveAll(memberCoupons);
+
         return new ListResponse<>(memberCoupons.size(), memberCoupons.stream().map(MemberCouponResponse::from).toList());
     }
+
+    private void validateRequestedCouponsExist(MemberCouponUseRequest request, List<MemberCoupon> memberCoupons) {
+        List<Long> missingCouponIds = request.getMemberCouponIds().stream()
+                .filter(requestedCouponId -> memberCoupons.stream().noneMatch(memberCoupon -> memberCoupon.getId().equals(requestedCouponId))).toList();
+
+        if (!missingCouponIds.isEmpty()) {
+            throw new RuntimeException("사용할 수 있는 쿠폰 중에 쿠폰 ID " + missingCouponIds + "에 해당하는 쿠폰이 없습니다.");
+        }
+    }
+
 
     public MemberCouponResponse generateMemberCoupon(Long memberId, MemberCouponRequest request) {
         Member member = memberRepository.findById(memberId).orElseThrow(() -> new RuntimeException("Member not exist."));
