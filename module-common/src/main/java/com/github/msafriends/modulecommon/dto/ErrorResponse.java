a40ae923a0f6e67364f5c32cb2f3699f86da636b
oereo <dlstpgns0406@gmail.com>
2023-07-29 03:23:54 +0900

    refactor: ErrorResponse 객체 생성시에 BusinessException detail field에 대해 받을 수 있도록 method 추가

--- module-common/src/main/java/com/github/msafriends/modulecommon/dto/ErrorResponse.java
+++ module-common/src/main/java/com/github/msafriends/modulecommon/dto/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.github.msafriends.modulecommon.dto;
 
+import lombok.Builder;
 import org.springframework.http.HttpStatus;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -17,13 +18,26 @@ public class ErrorResponse {
 	@JsonIgnore
 	private HttpStatus status;
 
-	private ErrorResponse(ErrorCode errorCode) {
-		this.status = errorCode.getStatus();
-		this.code = errorCode.getCode();
-		this.message = errorCode.getMessage();
+	@Builder
+	private ErrorResponse(HttpStatus status,String code, String message) {
+		this.status = status;
+		this.code = code;
+		this.message = message;
 	}
 
 	public static ErrorResponse of(ErrorCode errorCode){
-		return new ErrorResponse(errorCode);
+		return ErrorResponse.builder()
+				.status(errorCode.getStatus())
+				.code(errorCode.getCode())
+				.message(errorCode.getMessage())
+				.build();
+	}
+
+	public static ErrorResponse of(ErrorCode errorCode, String message) {
+		return ErrorResponse.builder()
+				.status(errorCode.getStatus())
+				.code(errorCode.getCode())
+				.message(message)
+				.build();
 	}
 }
