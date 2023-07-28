da33fcc788f80cb030fd029f85ad604a00f7808a
oereo <dlstpgns0406@gmail.com>
2023-07-28 15:14:57 +0900

    test: IntegrationTestSupport abstract class 추가

---
+++ service-member/module-api/src/test/java/com/github/msafriends/moduleapi/common/IntegrationTestSupport.java
@@ -0,0 +1,9 @@
+package com.github.msafriends.moduleapi.common;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("local")
+public abstract class IntegrationTestSupport {
+}
