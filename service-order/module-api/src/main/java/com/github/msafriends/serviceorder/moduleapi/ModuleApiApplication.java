288a2f772585256f0c17005ff6f01691bc86b669
BINARYBARD <binarybard@pm.me>
2023-07-28 15:42:14 +0900

    refactor: 감사 관련 애노테이션을 별도의 설정 클래스로 분리

--- service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/ModuleApiApplication.java
+++ service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/ModuleApiApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableDiscoveryClient
-@EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = {"com.github.msafriends.modulecommon", "com.github.msafriends.serviceorder.moduleapi"})
 @EntityScan(basePackages = "com.github.msafriends.serviceorder.modulecore.domain")
 public class ModuleApiApplication {
