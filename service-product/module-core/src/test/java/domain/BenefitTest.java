8bc69b1c74dad17c59b0c8b809433d90b4f3c4f2
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-28 19:35:28 +0900

    refactor : Product 엔티티 변경으로 인한 Product테스트 재작성

--- service-product/module-core/src/test/java/domain/BenefitTest.java
+++ service-product/module-core/src/test/java/domain/BenefitTest.java
@@ -1,10 +1,12 @@
-package domain.product;
+package domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BenefitTest {
+import domain.product.Benefit;
+
+class BenefitTest {
 
     @Test
     @DisplayName("할인이 0과 100 사이가 아닌 경우 예외가 발생한다.")
@@ -12,12 +14,12 @@ public class BenefitTest {
         Assertions.assertThatThrownBy(() -> Benefit.builder()
                 .discount(-1)
                 .build()).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("할인은 0과 100사이입니다.");
+                .hasMessage("할인율은 0과 100사이입니다.");
 
         Assertions.assertThatThrownBy(() -> Benefit.builder()
                         .discount(101)
                         .build()).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("할인은 0과 100사이입니다.");
+                .hasMessage("할인율은 0과 100사이입니다.");
     }
 
     @Test
