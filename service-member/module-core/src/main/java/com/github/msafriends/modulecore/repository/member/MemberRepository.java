58ab1a228fae4c4153cca6c4c3ebd30f65f94196
oereo <dlstpgns0406@gmail.com>
2023-07-28 15:19:11 +0900

    feat: findByEmail method 추가

--- service-member/module-core/src/main/java/com/github/msafriends/modulecore/repository/member/MemberRepository.java
+++ service-member/module-core/src/main/java/com/github/msafriends/modulecore/repository/member/MemberRepository.java
@@ -1,9 +1,13 @@
 package com.github.msafriends.modulecore.repository.member;
 
+import com.github.msafriends.modulecore.domain.member.Email;
 import com.github.msafriends.modulecore.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(Email email);
 }
