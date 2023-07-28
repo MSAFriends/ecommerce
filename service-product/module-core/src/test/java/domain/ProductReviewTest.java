7e2ff3440874af033c1a012d11b1ac945b6c69eb
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-28 19:42:05 +0900

    refactor : ProductReview엔티티 테스트코드 리팩토링

--- service-product/module-core/src/test/java/domain/ProductReviewTest.java
+++ service-product/module-core/src/test/java/domain/ProductReviewTest.java
@@ -1,14 +1,14 @@
-package domain.review;
+package domain;
 
-import domain.product.Benefit;
 import domain.product.Product;
-import fixture.product.BenefitFixture;
+import domain.review.ProductReview;
 import fixture.product.ProductFixture;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ReviewTest {
+public class ProductReviewTest {
     private static final Long MEMBER_ID = 1L;
 
     private static final int RATING = 5;
@@ -18,28 +18,26 @@ public class ReviewTest {
     @Test
     @DisplayName("리뷰 등록이 성공적으로 이루어 진다.")
     void successRegisterReview() {
-        Benefit benefit = BenefitFixture.createDefaultBenefit();
-        Product product = ProductFixture.createProduct(benefit);
-        Review review = Review.builder()
+        Product product = ProductFixture.createProduct();
+        ProductReview productReview = ProductReview.builder()
                 .memberId(MEMBER_ID)
                 .product(product)
                 .rating(RATING)
                 .title(TITLE)
                 .content(CONTENT)
                 .build();
-        Assertions.assertThat(review.getMemberId()).isEqualTo(MEMBER_ID);
-        Assertions.assertThat(review.getRating()).isEqualTo(RATING);
-        Assertions.assertThat(review.getTitle()).isEqualTo(TITLE);
-        Assertions.assertThat(review.getContent()).isEqualTo(CONTENT);
+        Assertions.assertThat(productReview.getMemberId()).isEqualTo(MEMBER_ID);
+        Assertions.assertThat(productReview.getRating()).isEqualTo(RATING);
+        Assertions.assertThat(productReview.getTitle()).isEqualTo(TITLE);
+        Assertions.assertThat(productReview.getContent()).isEqualTo(CONTENT);
     }
 
     @Test
     @DisplayName("리뷰 등록시 평점을 0 ~ 5점 사이로 하지 않으면 예외가 발생한다.")
     void failRegisterReviewWithInvalidRating() {
-        Benefit benefit = BenefitFixture.createDefaultBenefit();
-        Product product = ProductFixture.createProduct(benefit);
+        Product product = ProductFixture.createProduct();
         Assertions.assertThatThrownBy(() -> {
-            Review.builder()
+            ProductReview.builder()
                     .memberId(MEMBER_ID)
                     .product(product)
                     .rating(6)
