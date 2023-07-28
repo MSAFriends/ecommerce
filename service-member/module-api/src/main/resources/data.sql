ae8156b7aec3d341d0e24821b0201a8ffc601210
oereo <dlstpgns0406@gmail.com>
2023-07-28 16:11:58 +0900

    feat: Coupon class에 쿠폰의 유효기간 설정에 필요한 validationRange field를 추가하고 그에 따른 로직 반영

--- service-member/module-api/src/main/resources/data.sql
+++ service-member/module-api/src/main/resources/data.sql
@@ -1,5 +1,6 @@
 -- Coupon
 
-INSERT INTO coupons (name, discount_type, value, generate_type, max_quantity_per_member, quantity) VALUES ('1000원 할인 쿠폰', 'FIXED', 1000, 'SIGNUP_THANKS', 1, 1);
-INSERT INTO coupons (name, discount_type, value, generate_type, max_quantity_per_member, quantity) VALUES ('10% 할인 쿠폰', 'PERCENTAGE', 10, 'SIGNUP_THANKS', 1, 1)
+INSERT INTO coupons (name, discount_type, value, generate_type, max_quantity_per_member, quantity, validation_range) VALUES ('1000원 할인 쿠폰', 'FIXED', 1000, 'SIGNUP_THANKS', 1, 1, 30);
+INSERT INTO coupons (name, discount_type, value, generate_type, max_quantity_per_member, quantity, validation_range) VALUES ('10% 할인 쿠폰', 'PERCENTAGE', 10, 'SIGNUP_THANKS', 1, 1, 30);
+INSERT INTO coupons (name, discount_type, value, generate_type, max_quantity_per_member, quantity, validation_range) VALUES ('15% 할인 쿠폰', 'PERCENTAGE', 15, 'DAILY_UNLIMITED', 1, 1, 10);
 
