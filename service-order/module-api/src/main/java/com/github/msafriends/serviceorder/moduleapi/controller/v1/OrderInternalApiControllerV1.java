17056d13a889be9ab4b3a92f7d69bb4a9baf9ef9
BINARYBARD <binarybard@pm.me>
2023-07-28 17:25:21 +0900

    feat: 현재 멤버의 장바구니 조회 API 추가

--- service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/controller/v1/OrderInternalApiControllerV1.java
+++ service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/controller/v1/OrderInternalApiControllerV1.java
@@ -1,6 +1,7 @@
 package com.github.msafriends.serviceorder.moduleapi.controller.v1;
 
 import com.github.msafriends.serviceorder.modulecore.dto.request.order.UpdateCartItemRequest;
+import com.github.msafriends.serviceorder.modulecore.dto.response.order.OrderCartResponse;
 import com.github.msafriends.serviceorder.modulecore.dto.response.order.OrderResponse;
 import com.github.msafriends.serviceorder.moduleapi.service.OrderService;
 import jakarta.validation.Valid;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +27,11 @@ public class OrderInternalApiControllerV1 {
     @GetMapping("/members/{memberId}/orders")
     public ResponseEntity<List<OrderResponse>> getOrders(@PathVariable("memberId") Long memberId) {
         return ResponseEntity.ok(orderService.getOrdersByMemberId(memberId));
+    }
+
+    @GetMapping("/orders/carts")
+    public ResponseEntity<Optional<OrderCartResponse>> getCartItems(@RequestHeader("Member-Id") Long memberId) {
+        return ResponseEntity.ok(orderService.getCartItemsByMemberId(memberId));
     }
 
     @PostMapping("/orders")
