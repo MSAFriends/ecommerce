3ff11cd7727f74b52e6575bd537abdb22ed5314a
oereo <dlstpgns0406@gmail.com>
2023-07-28 18:55:31 +0900

    refactor: 상수 변수에 대해 대문자로 naming 수정 및 nameValidator 검증 시에 영어 소문자로만 이루어질 수 있도록 검증

--- service-member/module-api/src/main/java/com/github/msafriends/moduleapi/validator/MemberNameValidator.java
+++ service-member/module-api/src/main/java/com/github/msafriends/moduleapi/validator/MemberNameValidator.java
@@ -4,13 +4,19 @@ import com.github.msafriends.moduleapi.validator.anntations.ValidMemberName;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+import java.util.regex.Pattern;
+
 public class MemberNameValidator implements ConstraintValidator<ValidMemberName, String> {
+    private static final String NAME_REGEX = "^[a-z]+$";
     private static final int MIN_NAME_SIZE = 5;
     private static final int MAX_NAME_SIZE = 20;
 
     @Override
     public boolean isValid(String name, ConstraintValidatorContext context) {
         if (name == null) {
+            return false;
+        }
+        if (!Pattern.matches(NAME_REGEX, name)) {
             return false;
         }
         return MIN_NAME_SIZE <= name.length() && name.length() <= MAX_NAME_SIZE;
