48f09ce9a98dbce9d0041a8d9be0e7cb4c27703c
oereo <dlstpgns0406@gmail.com>
2023-07-28 15:22:15 +0900

    feat: findCouponByGenerateType method 구현

--- service-member/module-core/src/main/java/com/github/msafriends/modulecore/repository/coupon/CouponRepository.java
+++ service-member/module-core/src/main/java/com/github/msafriends/modulecore/repository/coupon/CouponRepository.java
@@ -1,9 +1,13 @@
 package com.github.msafriends.modulecore.repository.coupon;
 
 import com.github.msafriends.modulecore.domain.coupon.Coupon;
+import com.github.msafriends.modulecore.domain.coupon.CouponGenerateType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    List<Coupon> findCouponByGenerateType(CouponGenerateType couponGenerateType);
 }
