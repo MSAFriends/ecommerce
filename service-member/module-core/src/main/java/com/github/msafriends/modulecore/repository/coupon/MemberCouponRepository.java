155cdf70aaf1b3b663d3a7ee4fe3601f40891e2f
oereo <dlstpgns0406@gmail.com>
2023-07-28 15:20:42 +0900

    feat: findMemberCouponsByMember method 구현

--- service-member/module-core/src/main/java/com/github/msafriends/modulecore/repository/coupon/MemberCouponRepository.java
+++ service-member/module-core/src/main/java/com/github/msafriends/modulecore/repository/coupon/MemberCouponRepository.java
@@ -1,9 +1,13 @@
 package com.github.msafriends.modulecore.repository.coupon;
 
 import com.github.msafriends.modulecore.domain.coupon.MemberCoupon;
+import com.github.msafriends.modulecore.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long> {
+    List<MemberCoupon> findMemberCouponsByMember(Member member);
 }
