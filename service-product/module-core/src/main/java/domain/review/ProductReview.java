c09838e41a91c726cf7df1608760f72df4900194
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-28 19:41:34 +0900

    feat : 기존의 Review 엔티티 ProductReview로 수정

--- service-product/module-core/src/main/java/domain/review/ProductReview.java
+++ service-product/module-core/src/main/java/domain/review/ProductReview.java
@@ -9,68 +9,56 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.util.Assert;
 
-import java.time.LocalDateTime;
+import org.springframework.util.Assert;
 
 import static lombok.AccessLevel.*;
 
-@Entity
-@NoArgsConstructor(access = PROTECTED)
-@Getter
-public class Review {
+import com.github.msafriends.modulecommon.base.BaseTimeEntity;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@DiscriminatorValue("PRODUCT")
+public class ProductReview extends BaseTimeEntity {
     @Id
     @GeneratedValue
     @Column(name = "review_id")
     private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
-    private Product product;
-
-    @NotNull
-    private Long memberId;
-
     @Min(value = 0, message = "0점 이상의 점수를 입력해주세요")
     @Max(value = 5, message = "5점 이하의 점수를 입력해주세요")
     private int rating;
-
     @Column(nullable = false, length = 50)
     private String title;
-
     @Column(nullable = false, length = 500)
     private String content;
-
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @NotNull
+    private Long memberId;
 
     @Builder
-    public Review(Long memberId, Product product, int rating, String title, String content) {
-        validateReview(memberId, title, content, rating);
-        this.memberId = memberId;
-        this.product = product;
+    public ProductReview(int rating, String title, String content, Product product, Long memberId) {
+        validateProductReview(rating, product, memberId, title, content);
         this.rating = rating;
         this.title = title;
         this.content = content;
+        this.product = product;
+        this.memberId = memberId;
     }
 
-    private void validateReview(Long memberId, String title, String content, int rating) {
-        validateNotNull(memberId, title, content);
+    private void validateProductReview(int rating, Product product, Long memberId, String title, String content){
+        validateNotNull(product, memberId, title, content);
         validateRating(rating);
     }
-
     private void validateRating(int rating) {
         Assert.isTrue(rating >= 0 && rating <= 5, "평점은 0 ~ 5점 사이로 입력해주세요.");
     }
 
-    private void validateNotNull(Long memberId, String title, String content) {
-        Assert.notNull(memberId, "title must not be null");
+    private void validateNotNull(Product product, Long memberId, String title, String content){
+        Assert.notNull(product, "Product must not be null");
+        Assert.notNull(memberId, "memberId must not be null");
         Assert.notNull(title, "title must not be null");
         Assert.notNull(content, "content must not be null");
     }
