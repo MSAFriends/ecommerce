fe2be9161bbf49713d30861787d6b95c650bb29f
oereo <dlstpgns0406@gmail.com>
2023-07-28 15:14:37 +0900

    chore: data.sql을 통한 Coupon data 작업 추가

---
+++ service-member/module-api/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- Coupon
+
+INSERT INTO coupons (name, discount_type, value, generate_type, max_quantity_per_member, quantity) VALUES ('1000원 할인 쿠폰', 'FIXED', 1000, 'SIGNUP_THANKS', 1, 1);
+INSERT INTO coupons (name, discount_type, value, generate_type, max_quantity_per_member, quantity) VALUES ('10% 할인 쿠폰', 'PERCENTAGE', 10, 'SIGNUP_THANKS', 1, 1)
+
