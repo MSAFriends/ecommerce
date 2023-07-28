47b599a5d689b86bf972fddca437b8b1c17b446d
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-28 19:43:28 +0900

    feat : Product 등록 엔드 포인트 추가 및 테스트 코드 작성

---
+++ service-product/module-api/src/test/java/common/AcceptanceTest.java
@@ -0,0 +1,15 @@
+package common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.github.msafriends.serviceproduct.moduleapi.ModuleApiApplication;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = ModuleApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class AcceptanceTest {
+	@Autowired
+	protected MockMvc mockMvc;
+}
