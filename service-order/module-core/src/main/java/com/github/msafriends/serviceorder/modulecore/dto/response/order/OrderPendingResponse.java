a6638272c79e519cbc477fc581b97fc668a4ac85
BINARYBARD <binarybard@pm.me>
2023-07-29 17:48:37 +0900

    rename: 통일성을 위해 DTO 객체의 이름을 변경함

--- service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/dto/response/order/OrderPendingResponse.java
+++ service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/dto/response/order/OrderPendingResponse.java
@@ -7,22 +7,22 @@ import lombok.Getter;
 import java.util.List;
 
 @Getter
-public class PendingOrderResponse {
+public class OrderPendingResponse {
     private final Long id;
     private final Long memberId;
     private final PriceResponse price;
     private final List<CartItemResponse> cartItems;
 
     @Builder
-    public PendingOrderResponse(Long id, Long memberId, PriceResponse price, List<CartItemResponse> cartItems) {
+    public OrderPendingResponse(Long id, Long memberId, PriceResponse price, List<CartItemResponse> cartItems) {
         this.id = id;
         this.memberId = memberId;
         this.price = price;
         this.cartItems = cartItems;
     }
 
-    public static PendingOrderResponse from(final Order order) {
-        return PendingOrderResponse.builder()
+    public static OrderPendingResponse from(final Order order) {
+        return OrderPendingResponse.builder()
                 .id(order.getId())
                 .memberId(order.getMemberId())
                 .price(PriceResponse.from(order))
