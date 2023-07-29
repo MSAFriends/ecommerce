c59f4a12f44e236b3105face5cf570412019ac84
BINARYBARD <binarybard@pm.me>
2023-07-29 10:33:15 +0900

    feat: 주문을 확정하는 API 엔드포인트를 추가함

--- service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/service/OrderService.java
+++ service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/service/OrderService.java
@@ -1,15 +1,20 @@
 package com.github.msafriends.serviceorder.moduleapi.service;
 
+import com.github.msafriends.serviceorder.moduleapi.client.MemberServiceClient;
 import com.github.msafriends.serviceorder.moduleapi.repository.OrderRepository;
+import com.github.msafriends.serviceorder.modulecore.domain.coupon.OrderCoupon;
 import com.github.msafriends.serviceorder.modulecore.domain.order.Order;
 import com.github.msafriends.serviceorder.modulecore.domain.order.OrderStatus;
+import com.github.msafriends.serviceorder.modulecore.dto.request.order.ConfirmOrderRequest;
 import com.github.msafriends.serviceorder.modulecore.dto.request.order.UpdateCartItemRequest;
+import com.github.msafriends.serviceorder.modulecore.dto.response.coupon.OrderCouponResponse;
 import com.github.msafriends.serviceorder.modulecore.dto.response.order.OrderCartResponse;
 import com.github.msafriends.serviceorder.modulecore.dto.response.order.OrderResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +22,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final MemberServiceClient memberServiceClient;
 
     public OrderResponse getOrder(Long orderId) {
         return orderRepository.findById(orderId)
@@ -40,6 +46,28 @@ public class OrderService {
         Order pendingOrder = getOrCreatePendingOrder(memberId);
         pendingOrder.updateCartItem(request);
         return orderRepository.save(pendingOrder).getId();
+    }
+
+    public void confirmOrder(Long memberId, ConfirmOrderRequest request) {
+        Order order = orderRepository.findByMemberIdAndStatus(memberId, OrderStatus.PENDING)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
+        order.confirm(request);
+        applyCoupons(order, request.getOrderCouponIds());
+        orderRepository.save(order);
+    }
+
+    private void applyCoupons(Order order, List<Long> orderCouponIds) {
+        List<OrderCouponResponse> availableCoupons = memberServiceClient.getAvailableCouponsByMemberId(order.getMemberId());
+        List<OrderCoupon> appliedCoupons = new ArrayList<>();
+
+        orderCouponIds.forEach(couponId -> {
+            OrderCouponResponse coupon = availableCoupons.stream()
+                    .filter(c -> c.getId().equals(couponId))
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Coupon not found"));
+            appliedCoupons.add(OrderCouponResponse.toCoupon(order, coupon));
+        });
+        order.addCoupons(appliedCoupons);
     }
 
     @Transactional
