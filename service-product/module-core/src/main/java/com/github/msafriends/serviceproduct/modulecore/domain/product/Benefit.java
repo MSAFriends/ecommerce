5833658c098e430ef288cca5505b807bb74de0e8
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-29 16:45:15 +0900

    refactor: 매직넘버 상수로 대체

--- service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/domain/product/Benefit.java
+++ service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/domain/product/Benefit.java
@@ -1,4 +1,4 @@
-package domain.product;
+package com.github.msafriends.serviceproduct.modulecore.domain.product;
 
 import static lombok.AccessLevel.*;
 
@@ -13,6 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = PROTECTED)
 @Getter
 public class Benefit {
+    private static final int MAX_DISCOUNT_RATE = 100;
+
     private int discount;
     private int mileage;
 
@@ -29,7 +31,7 @@ public class Benefit {
     }
 
     private void validateDiscount(int discount) {
-        Assert.isTrue(discount >= 0 && discount <= 100, "할인율은 0과 100사이입니다.");
+        Assert.isTrue(discount >= 0 && discount <= MAX_DISCOUNT_RATE, "할인율은 0과 100사이입니다.");
     }
 
     private void validateMileage(int mileage) {
