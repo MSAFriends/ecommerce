db87dca82a9fa549223e6f92982004e306ea6791
oereo <dlstpgns0406@gmail.com>
2023-08-03 01:32:39 +0900

    refactor: remove unused modules

--- service-member/module-api/src/main/java/com/github/msafriends/moduleapi/ModuleApiApplication.java
+++ service-member/module-api/src/main/java/com/github/msafriends/moduleapi/ModuleApiApplication.java
@@ -1,12 +1,9 @@
 package com.github.msafriends.moduleapi;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Slf4j
