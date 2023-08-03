8a68ee8286ae15773c4b851b189c3d1d57c2d378
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-03 09:42:57 +0900

    feat : module-core의 repository bean을 scan하기 위한 EnableJpaRepositories annotation추가

--- service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/ModuleApiApplication.java
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/ModuleApiApplication.java
@@ -3,9 +3,11 @@ package com.github.msafriends.serviceproduct.moduleapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com/github/msafriends/serviceproduct/modulecore/domain")
+@EnableJpaRepositories("com.github.msafriends.serviceproduct.modulecore.repository")
+@EntityScan(basePackages = "com.github.msafriends.serviceproduct.modulecore.domain")
 public class ModuleApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ModuleApiApplication.class, args);
