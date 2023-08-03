dc0f2441811f434b6be768ee2f17f6fe59560d2d
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-03 23:06:18 +0900

    refactor: 불필요한 import문 제거

--- service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/controller/external/v1/BatchExternalControllerV1.java
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/controller/external/v1/BatchExternalControllerV1.java
@@ -1,6 +1,5 @@
 package com.github.msafriends.serviceproduct.moduleapi.controller.external.v1;
 
-import org.hibernate.engine.jdbc.batch.spi.Batch;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
