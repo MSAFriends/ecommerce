ff0a48fdff8c0d18bd5b833b5b41792ae898c1d0
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-03 23:05:47 +0900

    feat : Product, ProductImage 도메인 data migration엔드 포인트 추가

---
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/controller/external/v1/BatchExternalControllerV1.java
@@ -0,0 +1,26 @@
+package com.github.msafriends.serviceproduct.moduleapi.controller.external.v1;
+
+import org.hibernate.engine.jdbc.batch.spi.Batch;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.msafriends.serviceproduct.moduleapi.service.BatchService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/batch")
+public class BatchExternalControllerV1 {
+    private final BatchService batchService;
+
+    @PostMapping("/init/migration")
+    public ResponseEntity<String> runJob() throws Exception{
+        batchService.startElevenStreetProductAndProductImageDataMigration();
+        return ResponseEntity.ok("ok");
+    }
+}
