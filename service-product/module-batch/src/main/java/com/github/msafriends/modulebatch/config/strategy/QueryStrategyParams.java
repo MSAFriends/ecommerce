845fa91629ce671c586323f2df35689a831f18d6
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-04 09:49:16 +0900

    fix: MERGE문 활용을 위한 unique key 필드 추가

--- service-product/module-batch/src/main/java/com/github/msafriends/modulebatch/config/strategy/QueryStrategyParams.java
+++ service-product/module-batch/src/main/java/com/github/msafriends/modulebatch/config/strategy/QueryStrategyParams.java
@@ -8,11 +8,16 @@ public class QueryStrategyParams {
     private final String tableName;
     private final String[] columnNames;
     private final String[] columnValues;
+    private final String uniqueColumnName;
+    private final String uniqueColumnValue;
 
     @Builder
-    public QueryStrategyParams(String tableName, String[] columnNames, String[] columnValues) {
+    public QueryStrategyParams(String tableName, String[] columnNames, String[] columnValues, String uniqueColumnName,
+        String uniqueColumnValue) {
         this.tableName = tableName;
         this.columnNames = columnNames;
         this.columnValues = columnValues;
+        this.uniqueColumnName = uniqueColumnName;
+        this.uniqueColumnValue = uniqueColumnValue;
     }
 }
