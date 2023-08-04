5b93475a5022c17daf659cd58d79c3b52eacf69f
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-04 10:15:39 +0900

    fix: DB별 queryStrategy uniqueColumn, value 필드 추가

--- service-product/module-batch/src/main/java/com/github/msafriends/modulebatch/config/strategy/H2QueryStrategy.java
+++ service-product/module-batch/src/main/java/com/github/msafriends/modulebatch/config/strategy/H2QueryStrategy.java
@@ -4,17 +4,23 @@ public class H2QueryStrategy implements QueryStrategy{
     private final String tableName;
     private final String[] columnNames;
     private final String[] columnValues;
+    private final String uniqueColumnName;
 
     public H2QueryStrategy(final QueryStrategyParams queryStrategyParams) {
         this.tableName = queryStrategyParams.getTableName();
         this.columnNames = queryStrategyParams.getColumnNames();
-        this.columnValues = queryStrategyParams.getColumnNames();
+        this.columnValues = queryStrategyParams.getColumnValues();
+        this.uniqueColumnName = queryStrategyParams.getUniqueColumnName();
     }
 
     @Override
     public String getInsertSQL() {
         String columns = String.join(", ", columnNames);
         String values = String.join(", ", columnValues);
-        return String.format("INSERT INTO %s (%s) VALUES (%s)", tableName, columns, values);
+        if(tableName.equals("products")){
+            return String.format("MERGE INTO %s (%s) KEY (%s) VALUES (%s)", tableName, columns, uniqueColumnName, values);
+        }else {
+            return String.format("INSERT INTO %s (%s) VALUES (%s)", tableName, columns, values);
+        }
     }
 }
