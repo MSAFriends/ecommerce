07765409b57a2e5b5b25927b72afdd0cf06b53da
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-04 17:31:44 +0900

    feat : 카테고리 등록 엔드포인트 추가

---
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/controller/internal/v1/CategoryInternalApiControllerV1.java
@@ -0,0 +1,33 @@
+package com.github.msafriends.serviceproduct.moduleapi.controller.internal.v1;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.msafriends.serviceproduct.moduleapi.dto.CategoryRequest;
+import com.github.msafriends.serviceproduct.moduleapi.service.CategoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/internal/v1")
+@RequiredArgsConstructor
+public class CategoryInternalApiControllerV1 {
+    private final CategoryService categoryService;
+
+    @PostMapping("/categories")
+    public ResponseEntity<Void> registerCategory(@RequestBody CategoryRequest request){
+        return ResponseEntity
+            .created(
+                URI.create(
+                    "/api/internal/v1/categories/"
+                        + categoryService.saveCategory(
+                            request.getParentCategoryId(), request.toCategory())
+                )
+            ).build();
+    }
+}
