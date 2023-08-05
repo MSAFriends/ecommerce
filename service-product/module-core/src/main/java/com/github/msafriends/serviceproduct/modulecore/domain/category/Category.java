092efbf82b693f184af3ac11ec92188a6cf28a9c
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-05 13:28:17 +0900

    feat : Category 셀프조인 연관관계 매핑 메소드 추가

--- service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/domain/category/Category.java
+++ service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/domain/category/Category.java
@@ -1,5 +1,7 @@
 package com.github.msafriends.serviceproduct.modulecore.domain.category;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.util.Assert;
 
 import jakarta.persistence.Column;
@@ -9,7 +11,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,15 +20,17 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(name = "categories")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "parent_id")
-	private Category parentCategory;
 	@Id
 	@Column(name = "category_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JoinColumn(name = "parent_id")
+	private Category parentCategory;
 	@Column(unique = true, nullable = false)
 	private String categoryName;
 
@@ -34,6 +39,10 @@ public class Category {
 		validateNotNull(categoryName);
 		this.categoryName = categoryName;
 		this.parentCategory = category;
+	}
+
+	public void assignParentCategory(Category parentCategory){
+		this.parentCategory = parentCategory;
 	}
 
 	private void validateNotNull(String categoryName){
