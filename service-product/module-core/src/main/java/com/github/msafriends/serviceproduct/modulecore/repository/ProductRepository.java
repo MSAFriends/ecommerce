e014213e4bca7c0215ed486ed107d74b34ca4581
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-05 22:05:46 +0900

    feat : 주문 id정보로 관련된 Product를 가져오는 메소드 정의

--- service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/repository/ProductRepository.java
+++ service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.github.msafriends.serviceproduct.modulecore.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.msafriends.serviceproduct.modulecore.domain.product.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    public List<Product> findProductsByIdIn(List<Long> ids);
 }
