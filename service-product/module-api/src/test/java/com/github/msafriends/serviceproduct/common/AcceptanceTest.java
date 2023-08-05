fd1e3a1edd7088520c924d1b41fc1ee5ca9294bf
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-05 13:24:46 +0900

    chores : AcceptanceTest 디렉토리 경로 이동

--- service-product/module-api/src/test/java/com/github/msafriends/serviceproduct/common/AcceptanceTest.java
+++ service-product/module-api/src/test/java/com/github/msafriends/serviceproduct/common/AcceptanceTest.java
@@ -1,4 +1,4 @@
-package common;
+package com.github.msafriends.serviceproduct.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
