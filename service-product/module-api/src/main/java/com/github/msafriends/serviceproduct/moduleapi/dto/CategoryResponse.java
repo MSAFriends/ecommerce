966c919c2a99015e5908cd59d8316b44f06ca274
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-05 17:55:56 +0900

    refactor: 서비스 로직 변경으로 인해 CategoryResponse에 parentCategoryId필드 추가

--- service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/dto/CategoryResponse.java
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/dto/CategoryResponse.java
@@ -1,5 +1,6 @@
 package com.github.msafriends.serviceproduct.moduleapi.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.github.msafriends.serviceproduct.modulecore.domain.category.Category;
@@ -7,25 +8,35 @@ import com.github.msafriends.serviceproduct.modulecore.domain.category.Category;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 public class CategoryResponse {
     private Long categoryId;
     private String categoryName;
-    private List<CategoryResponse> childCategories;
+    private Long parentCategoryId;
+    private List<CategoryResponse> childCategories = new ArrayList<>();
 
     @Builder
-    public CategoryResponse(Long categoryId, String categoryName, List<CategoryResponse> childCategories) {
+    public CategoryResponse(Long categoryId, String categoryName, Long parentCategoryId,
+        List<CategoryResponse> childCategories) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
-        this.childCategories = childCategories;
+        this.parentCategoryId = parentCategoryId;
+        if(childCategories != null){
+            this.childCategories = childCategories;
+        }
     }
 
-    public static CategoryResponse fromCategory(Category category){
-        return CategoryResponse.builder()
+    public static CategoryResponse from(Category category){
+        CategoryResponseBuilder builder = CategoryResponse.builder()
             .categoryId(category.getId())
-            .categoryName(category.getCategoryName())
-            .build();
+            .categoryName(category.getCategoryName());
+        if(category.getParentCategory() != null){
+            return builder.parentCategoryId(category.getParentCategory().getId())
+                .build();
+        }
+        return builder.build();
     }
 }
