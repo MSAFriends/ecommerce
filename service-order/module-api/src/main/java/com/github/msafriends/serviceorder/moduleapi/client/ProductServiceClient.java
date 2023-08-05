89656c36bc3f96fee2807845a4b915bc83923dcd
BINARYBARD <binarybard@pm.me>
2023-08-05 20:24:53 +0900

    chore: 상품 서비스에서 제공하는 API 선언

--- service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/client/ProductServiceClient.java
+++ service-order/module-api/src/main/java/com/github/msafriends/serviceorder/moduleapi/client/ProductServiceClient.java
@@ -1,12 +1,14 @@
 package com.github.msafriends.serviceorder.moduleapi.client;
 
-import com.github.msafriends.serviceorder.modulecore.dto.response.order.ProductResponse;
+import com.github.msafriends.serviceorder.modulecore.dto.request.order.UpdateStockRequest;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(name = "product-service")
 public interface ProductServiceClient {
-    @GetMapping("/api/v1/products/{productId}")
-    ProductResponse getProduct(@PathVariable Long productId);
+    @PostMapping("/api/internal/v1/products/stocks")
+    void bulkUpdateProductStocks(@RequestBody List<UpdateStockRequest> requests);
 }
