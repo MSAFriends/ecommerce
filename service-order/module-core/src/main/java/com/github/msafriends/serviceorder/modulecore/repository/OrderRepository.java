321de390de15cf24eccf4b32207d3b9dc3228097
BINARYBARD <binarybard@pm.me>
2023-08-06 16:43:38 +0900

    feat: Optimistic Locking으로 장바구니에 상품 추가 시 발생하는 동시성 문제를 해결함

--- service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/repository/OrderRepository.java
+++ service-order/module-core/src/main/java/com/github/msafriends/serviceorder/modulecore/repository/OrderRepository.java
@@ -2,7 +2,9 @@ package com.github.msafriends.serviceorder.modulecore.repository;
 
 import com.github.msafriends.serviceorder.modulecore.domain.order.Order;
 import com.github.msafriends.serviceorder.modulecore.domain.order.OrderStatus;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +15,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findAllByMemberId(Long memberId);
 
+    @Lock(LockModeType.OPTIMISTIC)
     Optional<Order> findByMemberIdAndStatus(Long memberId, OrderStatus status);
 
     List<Order> findAllByMemberIdAndStatus(Long memberId, OrderStatus status);
