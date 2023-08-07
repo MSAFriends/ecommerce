a726614abf12cc4793734609d48641f9d829b731
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-07 17:38:32 +0900

    feat : PessimisticLock 도입을 위해 repository 메소드 추가, service클래스 추가

---
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/service/PessimisticLockProductService.java
@@ -0,0 +1,65 @@
+package com.github.msafriends.serviceproduct.moduleapi.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.msafriends.modulecommon.exception.EntityNotFoundException;
+import com.github.msafriends.modulecommon.exception.ErrorCode;
+import com.github.msafriends.serviceproduct.moduleapi.dto.UpdateStockRequest;
+import com.github.msafriends.serviceproduct.modulecore.domain.category.Category;
+import com.github.msafriends.serviceproduct.modulecore.domain.product.Product;
+import com.github.msafriends.serviceproduct.modulecore.repository.CategoryRepository;
+import com.github.msafriends.serviceproduct.modulecore.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Qualifier("pessimisticLockProductService")
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PessimisticLockProductService implements ProductService{
+	private final ProductRepository productRepository;
+	private final CategoryRepository categoryRepository;
+
+	@Transactional
+	public Long registerProduct(Long categoryId, Product product){
+		if(categoryId != null){
+			Category category = categoryRepository.findByIdOrThrow(categoryId);
+			product.assignCategory(category);
+		}
+		return productRepository.save(product).getId();
+	}
+
+	@Transactional
+	public Long registerProduct(Product product){
+		return productRepository.save(product).getId();
+	}
+
+	@Transactional
+	public void updateStocks(List<UpdateStockRequest> updateStockRequests){
+		List<Long> orderedIds = updateStockRequests.stream()
+            .map(UpdateStockRequest::getProductId)
+			.toList();
+		Map<Long, Integer> requestMap = updateStockRequests.stream()
+			.collect(Collectors.toMap(UpdateStockRequest::getProductId, UpdateStockRequest::getQuantity));
+		List<Product> foundProducts = productRepository.findProductsByIdInWithPessimisticLock(orderedIds);
+		if(orderedIds.size() != foundProducts.size())
+			throw new EntityNotFoundException(ErrorCode.INVALID_ORDER_ERROR, "유효하지 않은 상품 id가 주문에 포함되어 있습니다.");
+		foundProducts.forEach(product -> product.updateStockQuantity(requestMap.get(product.getId())));
+	}
+
+	@Override
+	public List<Product> readProductsBySellerId(Long sellerId) {
+		return null;
+	}
+
+	@Override
+	public List<Product> readProductByCategoryId(Long categoryId) {
+		return null;
+	}
+}
--- service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/repository/ProductRepository.java
+++ service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/repository/ProductRepository.java
@@ -3,15 +3,32 @@ package com.github.msafriends.serviceproduct.modulecore.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import com.github.msafriends.modulecommon.exception.EntityNotFoundException;
 import com.github.msafriends.modulecommon.exception.ErrorCode;
 import com.github.msafriends.serviceproduct.modulecore.domain.product.Product;
+
+import jakarta.persistence.LockModeType;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     default Product findByIdOrThrow(Long id){
         return findById(id)
             .orElseThrow(() -> new EntityNotFoundException(ErrorCode.PRODUCT_NOT_EXIST, id));
     }
-    public List<Product> findProductsByIdIn(List<Long> ids);
+
+    List<Product> findProductsBySellerId(Long sellerId);
+
+    List<Product> findProductsByIdIn(List<Long> ids);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.id in :ids")
+    List<Product> findProductsByIdInWithPessimisticLock(List<Long> ids);
+
+    @Query("SELECT p FROM Product p WHERE p.category=:categoryId")
+    List<Product> findProductByCategoryId(Long categoryId);
+
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("SELECT p FROM Product p WHERE p.id  in :ids")
+    List<Product> findProductsByIdInWithOptimisticLock(List<Long> ids);
 }
