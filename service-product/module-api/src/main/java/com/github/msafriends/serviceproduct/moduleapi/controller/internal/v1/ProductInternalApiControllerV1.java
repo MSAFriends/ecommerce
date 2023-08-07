5237781d08c5e0fc7af478b991d6d3df7d056135
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-07 17:42:02 +0900

    feat : Product조회 기능 추가 및 Lock을 위해 Facade의존성 추가

--- service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/controller/internal/v1/ProductInternalApiControllerV1.java
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/controller/internal/v1/ProductInternalApiControllerV1.java
@@ -3,8 +3,10 @@ package com.github.msafriends.serviceproduct.moduleapi.controller.internal.v1;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,16 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.msafriends.serviceproduct.moduleapi.dto.ProductRequest;
+import com.github.msafriends.serviceproduct.moduleapi.dto.ProductResponse;
 import com.github.msafriends.serviceproduct.moduleapi.dto.UpdateStockRequest;
 import com.github.msafriends.serviceproduct.moduleapi.service.ProductService;
-
-import lombok.RequiredArgsConstructor;
+import com.github.msafriends.serviceproduct.moduleapi.service.facade.LettuceLockProductFacade;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/internal/v1/products")
 public class ProductInternalApiControllerV1 {
+	private final LettuceLockProductFacade productFacade;
 	private final ProductService productService;
+
+	public ProductInternalApiControllerV1(LettuceLockProductFacade productFacade, @Qualifier("defaultProductService") ProductService productService) {
+		this.productFacade = productFacade;
+		this.productService = productService;
+	}
 
 	@PostMapping
 	public ResponseEntity<Void> save(
@@ -38,7 +45,18 @@ public class ProductInternalApiControllerV1 {
 
 	@PostMapping("/stocks")
 	public ResponseEntity<Void> bulkUpdateProductStocks(@RequestBody List<UpdateStockRequest> requests){
-		productService.updateStocks(requests);
+		productFacade.updateStocks(requests);
 		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ProductResponse>> getProductsBySellerId(
+		@RequestHeader("Seller-Id") Long sellerId
+	){
+		List<ProductResponse> responses = productService.readProductsBySellerId(sellerId)
+			.stream()
+			.map(ProductResponse::from)
+			.toList();
+		return ResponseEntity.ok(responses);
 	}
 }
---
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/dto/ProductResponse.java
@@ -0,0 +1,60 @@
+package com.github.msafriends.serviceproduct.moduleapi.dto;
+
+import com.github.msafriends.serviceproduct.modulecore.domain.product.AgeLimit;
+import com.github.msafriends.serviceproduct.modulecore.domain.product.Product;
+import com.github.msafriends.serviceproduct.modulecore.domain.product.Size;
+
+import lombok.Builder;
+
+public class ProductResponse {
+    private Long productId;
+    private Long sellerId;
+    private Long code;
+    private String name;
+    private int price;
+    private int salesPrice;
+    private Integer quantity;
+    private String delivery;
+    private Integer buySatisfy;
+    private Integer discount;
+    private Integer mileage;
+    private AgeLimit ageLimit;
+    private Size size;
+    private Long categoryId;
+
+    @Builder
+    public ProductResponse(Long productId, Long sellerId, Long code, String name, int price, int salesPrice,
+        Integer quantity, String delivery, Integer buySatisfy, Integer discount, Integer mileage, AgeLimit ageLimit,
+        Size size, Long categoryId) {
+        this.productId = productId;
+        this.sellerId = sellerId;
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.salesPrice = salesPrice;
+        this.quantity = quantity;
+        this.delivery = delivery;
+        this.buySatisfy = buySatisfy;
+        this.discount = discount;
+        this.mileage = mileage;
+        this.ageLimit = ageLimit;
+        this.size = size;
+        this.categoryId = categoryId;
+    }
+
+    public static ProductResponse from(Product product){
+        return ProductResponse.builder()
+            .productId(product.getId())
+            .name(product.getName())
+            .price(product.getPrice().getPriceValue())
+            .salesPrice(product.getPrice().getSalePriceValue())
+            .quantity(product.getQuantity())
+            .code(product.getCode())
+            .categoryId(product.getCategory().getId())
+            .mileage(product.getBenefit().getMileage())
+            .ageLimit(product.getAgeLimit())
+            .discount(product.getBenefit().getDiscount())
+            .buySatisfy(product.getBuySatisfy())
+            .build();
+    }
+}
