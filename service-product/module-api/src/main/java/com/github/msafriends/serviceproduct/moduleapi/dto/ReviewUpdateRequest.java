cf6a59eb6db90b5f10a9c7e5a49fe242dfd27835
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-08 15:05:47 +0900

    feat : reviewUpdate관련 dto추가

--- service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/dto/ReviewUpdateRequest.java
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/dto/ReviewUpdateRequest.java
@@ -1,4 +1,36 @@
 package com.github.msafriends.serviceproduct.moduleapi.dto;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReviewUpdateRequest {
+    private static final int MIN_TITLE_LENGTH = 5;
+    private static final int MAX_TITLE_LENGTH = 50;
+    private static final int MAX_CONTENT_LENGTH = 500;
+    private static final int MAX_RATING_VALUE = 5;
+    @NotNull
+    @Range(min = 0, max = MAX_RATING_VALUE)
+    private Integer rating;
+    @NotBlank
+    @Length(min = MIN_TITLE_LENGTH, max = MAX_TITLE_LENGTH)
+    private String title;
+    @NotBlank
+    @Length(max = MAX_CONTENT_LENGTH)
+    private String content;
+
+    @Builder
+    public ReviewUpdateRequest(Integer rating, String title, String content) {
+        this.rating = rating;
+        this.title = title;
+        this.content = content;
+    }
 }
