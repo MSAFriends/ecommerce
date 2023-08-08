adca10cdea663b890574aa28514188d5144718aa
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-08 15:06:10 +0900

    feat : review 조회 관련 dto추가

---
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/dto/productreview/ProductReviewResponse.java
@@ -0,0 +1,34 @@
+package com.github.msafriends.serviceproduct.moduleapi.dto.productreview;
+
+import com.github.msafriends.serviceproduct.modulecore.domain.review.ProductReview;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductReviewResponse {
+    private Long productId;
+    private Integer rating;
+    private String title;
+    private String content;
+
+    @Builder
+    public ProductReviewResponse(Long productId, Integer rating, String title, String content) {
+        this.productId = productId;
+        this.rating = rating;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static ProductReviewResponse from(ProductReview productReview){
+        return ProductReviewResponse.builder()
+            .productId(productReview.getId())
+            .rating(productReview.getRating())
+            .title(productReview.getTitle())
+            .content(productReview.getContent())
+            .build();
+    }
+}
