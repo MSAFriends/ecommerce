01a335d240e63d061e95def2ad07938fbfa585d3
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-08 15:06:38 +0900

    feat : ProductReview id조회 메소드 추가

--- service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/repository/ProductReviewRepository.java
+++ service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/repository/ProductReviewRepository.java
@@ -5,9 +5,16 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.github.msafriends.modulecommon.exception.EntityNotFoundException;
+import com.github.msafriends.modulecommon.exception.ErrorCode;
 import com.github.msafriends.serviceproduct.modulecore.domain.review.ProductReview;
 
 public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
+
+    default ProductReview findByIdOrThrow(Long id){
+        return findById(id)
+            .orElseThrow(() -> new EntityNotFoundException(ErrorCode.REVIEW_NOT_EXIST, id));
+    }
 
     @Query("SELECT r FROM ProductReview r WHERE r.product = :productId")
     List<ProductReview> findAllByProductId(Long productId);
