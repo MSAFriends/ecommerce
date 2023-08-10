b9d694e8450b7bef1990a39b32370501b86ad231
Unan <unusualc@likelion.org>
2023-08-10 13:50:36 +0900

    [product-service] Add product image external api (#38)
    * feature: Add product image external api
    
    * fix: 리뷰 반영
    
    * test: product image external controller test code 작성

---
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/dto/ProductImageResponse.java
@@ -0,0 +1,39 @@
+package com.github.msafriends.serviceproduct.moduleapi.dto;
+
+import com.github.msafriends.serviceproduct.modulecore.domain.productimage.ProductImage;
+
+
+public record ProductImageResponse(
+        Long id,
+        String base,
+        String size100,
+        String size110,
+        String size120,
+        String size130,
+        String size140,
+        String size150,
+        String size170,
+        String size200,
+        String size250,
+        String size270,
+        String size300
+) {
+
+    public static ProductImageResponse from(ProductImage productImage) {
+        return new ProductImageResponse(
+                productImage.getId(),
+                productImage.getBase(),
+                productImage.getSize100(),
+                productImage.getSize110(),
+                productImage.getSize120(),
+                productImage.getSize130(),
+                productImage.getSize140(),
+                productImage.getSize150(),
+                productImage.getSize170(),
+                productImage.getSize200(),
+                productImage.getSize250(),
+                productImage.getSize270(),
+                productImage.getSize300()
+        );
+    }
+}
--- service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/repository/ProductRepository.java
+++ service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/repository/ProductRepository.java
@@ -1,11 +1,19 @@
 package com.github.msafriends.serviceproduct.modulecore.repository;
 
-import java.util.List;
 
+import com.github.msafriends.modulecommon.exception.EntityNotFoundException;
+import com.github.msafriends.modulecommon.exception.ErrorCode;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.msafriends.serviceproduct.modulecore.domain.product.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    default Product getProductByIdOrThrow(Long productId) {
+        return findById(productId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.PRODUCT_NOT_EXIST));
+    }
+
     public List<Product> findProductsByIdIn(List<Long> ids);
 }
