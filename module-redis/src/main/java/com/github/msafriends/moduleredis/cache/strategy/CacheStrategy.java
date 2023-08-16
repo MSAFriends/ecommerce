490d936aec7d0b5a547c25a8563bcf73b623d091
oereo <dlstpgns0406@gmail.com>
2023-08-16 12:38:35 +0900

    feat: CacheStrategy에 Cache interface 상속

--- module-redis/src/main/java/com/github/msafriends/moduleredis/cache/strategy/CacheStrategy.java
+++ module-redis/src/main/java/com/github/msafriends/moduleredis/cache/strategy/CacheStrategy.java
@@ -2,12 +2,11 @@ package com.github.msafriends.moduleredis.cache.strategy;
 
 import org.springframework.stereotype.Indexed;
 
+import javax.cache.Cache;
+
 @Indexed
-public interface CacheStrategy <KEY, VALUE> {
-    VALUE get(KEY key);
-    void put(KEY key, VALUE value);
-    void remove(KEY key);
+public interface CacheStrategy <K, V> extends Cache<K, V>{
     void clear();
     int getSize();
-    boolean contains(KEY key);
+    boolean contains(K key);
 }
