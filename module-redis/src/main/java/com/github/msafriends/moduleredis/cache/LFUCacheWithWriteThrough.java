37cbc53dc4edd85ec1d313d45d4867facea61a85
In_Sehoon <49235528+oereo@users.noreply.github.com>
2023-08-21 16:30:08 +0900

    [module-redis] Redis Config 정의 (#50)
    * feat: CacheStrategy에 Cache interface 상속
    
    * feat: RedisConfig class 정의
    
    * feat: RedissonConfig class 정의
    
    * feat: 다른 모듈에서 Redis Bean 설정하는 방법에 대한 description 추가
    
    * feat: CustomCachingManager class 정의와 캐싱 구현체 임시 주석 처리
    
    * refactor: remove unused modules

--- module-redis/src/main/java/com/github/msafriends/moduleredis/cache/LFUCacheWithWriteThrough.java
+++ module-redis/src/main/java/com/github/msafriends/moduleredis/cache/LFUCacheWithWriteThrough.java
@@ -1,7 +1,7 @@
-package com.github.msafriends.moduleredis.cache;
-
-import com.github.msafriends.moduleredis.cache.persistence.WriteThroughDataPersistenceStrategy;
-import com.github.msafriends.moduleredis.cache.strategy.LFUCacheStrategy;
-
-public class LFUCacheWithWriteThrough<K, V> implements LFUCacheStrategy<K, V>, WriteThroughDataPersistenceStrategy<K, V> {
-}
+//package com.github.msafriends.moduleredis.cache;
+//
+//import com.github.msafriends.moduleredis.cache.persistence.WriteThroughDataPersistenceStrategy;
+//import com.github.msafriends.moduleredis.cache.strategy.LFUCacheStrategy;
+//
+//public class LFUCacheWithWriteThrough<K, V> implements LFUCacheStrategy<K, V>, WriteThroughDataPersistenceStrategy<K, V> {
+//}
--- module-redis/src/main/java/com/github/msafriends/moduleredis/cache/TTLCacheWithReadWrite.java
+++ module-redis/src/main/java/com/github/msafriends/moduleredis/cache/TTLCacheWithReadWrite.java
@@ -1,7 +1,7 @@
-package com.github.msafriends.moduleredis.cache;
-
-import com.github.msafriends.moduleredis.cache.persistence.ReadWriteThroughDataPersistenceStrategy;
-import com.github.msafriends.moduleredis.cache.strategy.TimeToLiveCacheStrategy;
-
-public class TTLCacheWithReadWrite<K, V> implements TimeToLiveCacheStrategy<K, V>, ReadWriteThroughDataPersistenceStrategy<K, V> {
-}
+//package com.github.msafriends.moduleredis.cache;
+//
+//import com.github.msafriends.moduleredis.cache.persistence.ReadWriteThroughDataPersistenceStrategy;
+//import com.github.msafriends.moduleredis.cache.strategy.TimeToLiveCacheStrategy;
+//
+//public class TTLCacheWithReadWrite<K, V> implements TimeToLiveCacheStrategy<K, V>, ReadWriteThroughDataPersistenceStrategy<K, V> {
+//}
