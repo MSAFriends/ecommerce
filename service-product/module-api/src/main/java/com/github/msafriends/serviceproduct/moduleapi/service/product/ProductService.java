9ab52d5a5e2993304b2182098e624c82c754de0c
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-25 20:29:52 +0900

    refactor: 판매자별, 카테고리별 상품 조회 External API로 이동

--- service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/service/product/ProductService.java
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/service/product/ProductService.java
@@ -1,14 +1,69 @@
 package com.github.msafriends.serviceproduct.moduleapi.service.product;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-import com.github.msafriends.serviceproduct.moduleapi.dto.UpdateStockRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import com.github.msafriends.serviceproduct.modulecore.dto.product.UpdateStockRequest;
+import com.github.msafriends.serviceproduct.modulecore.domain.category.Category;
 import com.github.msafriends.serviceproduct.modulecore.domain.product.Product;
+import com.github.msafriends.serviceproduct.modulecore.exception.EntityNotFoundException;
+import com.github.msafriends.serviceproduct.modulecore.exception.ErrorCode;
+import com.github.msafriends.serviceproduct.modulecore.repository.CategoryRepository;
+import com.github.msafriends.serviceproduct.modulecore.repository.ProductRepository;
 
-public interface ProductService {
-    Long registerProduct(Long categoryId, Product product);
-    Long registerProduct(Product product);
-    void updateStocks(List<UpdateStockRequest> updateStockRequests);
-    List<Product>readProductsBySellerId(Long sellerId);
-    List<Product> readProductsByCategoryId(Long categoryId);
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ProductService {
+	private final ProductRepository productRepository;
+	private final CategoryRepository categoryRepository;
+
+
+	@Transactional
+	public Long registerProduct(Long categoryId, Product product){
+		if(categoryId != null){
+			Category category = categoryRepository.findByIdOrThrow(categoryId);
+			product.assignCategory(category);
+		}
+		return productRepository.save(product).getId();
+	}
+
+	@Transactional
+	public Long registerProduct(Product product){
+		return productRepository.save(product).getId();
+	}
+
+	@Async
+	@Transactional
+	public void updateStocks(List<UpdateStockRequest> updateStockRequests){
+		List<Long> orderedIds = updateStockRequests.stream()
+            .map(UpdateStockRequest::getProductId)
+			.toList();
+		Map<Long, Integer> requestMap = updateStockRequests.stream()
+			.collect(Collectors.toMap(UpdateStockRequest::getProductId, UpdateStockRequest::getQuantity));
+		List<Product> foundProducts = productRepository.findProductsByIdInWithPessimisticLock(orderedIds);
+		if(orderedIds.size() != foundProducts.size())
+			throw new EntityNotFoundException(ErrorCode.INVALID_ORDER_ERROR, "유효하지 않은 상품 id가 주문에 포함되어 있습니다.");
+		foundProducts.forEach(product -> product.updateStockQuantity(requestMap.get(product.getId())));
+	}
+
+
+	public Page<Product> readProductsBySellerId(Long sellerId, Pageable pageable) {
+		return productRepository.findProductsBySellerId(sellerId, pageable);
+	}
+
+
+	public Page<Product> readProductsByCategoryId(Long categoryId, Pageable pageable) {
+		return productRepository.findProductByCategoryId(categoryId, pageable);
+	}
 }
