758a4fba390e264ad3d5374720987f145525b76f
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-25 20:23:13 +0900

    refactor : 불필요한 클래스파일들 삭제

--- module-redis/src/main/java/com/github/msafriends/moduleredis/cache/FIFOCacheWithWriteBack.java
+++
@@ -1,7 +0,0 @@
-package com.github.msafriends.moduleredis.cache;
-
-import com.github.msafriends.moduleredis.cache.persistence.WriteBackDataPersistenceStrategy;
-import com.github.msafriends.moduleredis.cache.strategy.FIFOCacheStrategy;
-
-public class FIFOCacheWithWriteBack<K, V> implements FIFOCacheStrategy<K, V>, WriteBackDataPersistenceStrategy<K, V> {
-}
--- module-redis/src/main/java/com/github/msafriends/moduleredis/cache/LRUCacheWithReadThrough.java
+++
@@ -1,7 +0,0 @@
-package com.github.msafriends.moduleredis.cache;
-
-import com.github.msafriends.moduleredis.cache.persistence.ReadThroughDataPersistenceStrategy;
-import com.github.msafriends.moduleredis.cache.strategy.LRUCacheStrategy;
-
-public class LRUCacheWithReadThrough<K, V> implements LRUCacheStrategy<K, V>, ReadThroughDataPersistenceStrategy<K, V> {
-}
