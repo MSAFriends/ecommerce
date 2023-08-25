f9c28b584d6f1408e7bdfe14648257c5f549a64d
GangminRYOU <dbrkdals1994@gmail.com>
2023-08-25 20:30:32 +0900

    refactor: 판매자별, 카테고리별 상품 조회 External API로 이동으로 인한 메소드 반환값 변경

--- service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/repository/ProductRepository.java
+++ service-product/module-core/src/main/java/com/github/msafriends/serviceproduct/modulecore/repository/ProductRepository.java
@@ -2,6 +2,9 @@ package com.github.msafriends.serviceproduct.modulecore.repository;
 
 
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -18,7 +21,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             .orElseThrow(() -> new EntityNotFoundException(ErrorCode.PRODUCT_NOT_EXIST, id));
     }
 
-    List<Product> findTop1000ProductsBySellerId(Long sellerId);
+    Page<Product> findProductsBySellerId(Long sellerId, Pageable pageable);
 
     List<Product> findProductsByIdIn(List<Long> ids);
   
@@ -26,9 +29,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT p FROM Product p WHERE p.id in :ids")
     List<Product> findProductsByIdInWithPessimisticLock(List<Long> ids);
 
-    List<Product> findTop1000ProductByCategoryId(Long categoryId);
-
-    @Lock(LockModeType.OPTIMISTIC)
-    @Query("SELECT p FROM Product p WHERE p.id  in :ids")
-    List<Product> findProductsByIdInWithOptimisticLock(List<Long> ids);
+    Page<Product> findProductByCategoryId(Long categoryId, Pageable pageable);
 }
