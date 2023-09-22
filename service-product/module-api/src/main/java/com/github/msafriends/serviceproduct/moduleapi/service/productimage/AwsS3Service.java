680e89a73ba7f2964df60987b0131cb019cb40f7
GangminRYOU <dbrkdals1994@gmail.com>
2023-09-22 17:52:53 +0900

    refactor : 이미지 리사이징 로직 추가

--- service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/service/productimage/AwsS3Service.java
+++ service-product/module-api/src/main/java/com/github/msafriends/serviceproduct/moduleapi/service/productimage/AwsS3Service.java
@@ -1,16 +1,24 @@
 package com.github.msafriends.serviceproduct.moduleapi.service.productimage;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import net.coobird.thumbnailator.Thumbnailator;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
@@ -21,7 +29,11 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.util.IOUtils;
+import com.github.msafriends.serviceproduct.moduleapi.utils.ImageUtils;
 import com.github.msafriends.serviceproduct.modulecore.domain.productimage.ProductImage;
+import com.github.msafriends.serviceproduct.modulecore.domain.productimage.ResizedPixel;
+import com.github.msafriends.serviceproduct.modulecore.dto.productimage.CustomMultipartFile;
+import com.github.msafriends.serviceproduct.modulecore.dto.productimage.ProductImageResponse;
 import com.github.msafriends.serviceproduct.modulecore.exception.ErrorCode;
 import com.github.msafriends.serviceproduct.modulecore.exception.FileProcessingException;
 import com.github.msafriends.serviceproduct.modulecore.exception.InvalidValueException;
@@ -34,7 +46,9 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AwsS3Service {
+
 
     @Value("${images.path}")
     private String s3Path;
@@ -44,28 +58,43 @@ public class AwsS3Service {
     private final ProductRepository productRepository;
     private final ProductImageRepository productImageRepository;
 
-    public List<String> uploadFile(List<MultipartFile> multipartFiles, Long productId){
+
+    @Transactional
+    public ProductImageResponse uploadFile(MultipartFile multipartFile, Long productId){
         var product = productRepository.findByIdOrThrow(productId);
-        List<String> fileNameList = new ArrayList<>();
-        multipartFiles.forEach(file -> {
-            validateFile(file);
-            String filePath = createFilePath(file);
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentLength(file.getSize());
-            objectMetadata.setContentType(file.getContentType());
-            try(InputStream inputStream = file.getInputStream()){
-                amazonS3.putObject(new PutObjectRequest(bucket, filePath, inputStream, objectMetadata)
-                    .withCannedAcl(CannedAccessControlList.PublicRead));
-            }catch (IOException e){
-                throw new FileProcessingException(ErrorCode.FILE_PROCESSING_ERROR, "파일 쓰기에 실패하였습니다.");
-            }
-            fileNameList.add(filePath);
-        });
-        List<ProductImage> productImages = fileNameList.stream()
-            .map(fileName -> ProductImage.builder().product(product).base(fileName).build())
-            .toList();
-        productImageRepository.saveAll(productImages);
-        return fileNameList;
+        Map<String, String> fileNameMap = new HashMap<>();
+        validateFile(multipartFile);
+        try{
+            String hashedName = ImageUtils.hashFile(multipartFile.getBytes());
+            String fileExtension = ImageUtils.getFileExtension(multipartFile.getOriginalFilename());
+            String baseFilePath = ImageUtils.createBaseFilePath(s3Path, hashedName, fileExtension);
+            sendFileToS3(multipartFile, baseFilePath);
+            fileNameMap.put(ImageUtils.BASE_IMAGE_KEY, hashedName + ImageUtils.DOT + fileExtension);
+            List<CustomMultipartFile> resizedImages = createResizedImages(multipartFile, hashedName, fileExtension);
+            resizedImages.forEach(resizedImage -> sendFileToS3(resizedImage, resizedImage.getName()));
+            resizedImages.forEach(image -> fileNameMap.put(String.valueOf(image.getWidth()), image.getOriginalFilename()));
+        } catch (IOException ex){
+            throw new FileProcessingException(ErrorCode.FILE_PROCESSING_ERROR);
+        }
+        ProductImage productImage = ProductImage.createProductImageWithResize(product, fileNameMap);
+        ProductImage savedImage = productImageRepository.save(productImage);
+        return ProductImageResponse.from(savedImage);
+    }
+
+    private void sendFileToS3(MultipartFile file, String filePath) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+        try(InputStream inputStream = file.getInputStream()){
+            amazonS3.putObject(new PutObjectRequest(
+                bucket,
+                filePath,
+                inputStream,
+                objectMetadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException ex){
+            throw new FileProcessingException(ErrorCode.FILE_PROCESSING_ERROR);
+        }
     }
 
     public ByteArrayResource download(String fileKey){
@@ -81,14 +110,55 @@ public class AwsS3Service {
         return new ByteArrayResource(bytes);
     }
 
+    @Transactional
     public void deleteFile(String fileName, Long productImageId){
         amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
         productImageRepository.deleteById(productImageId);
     }
 
+    public List<CustomMultipartFile> createResizedImages(MultipartFile file, String fileName, String fileExtension) throws
+        IOException {
+        BufferedImage image = ImageIO.read(file.getInputStream());
+        return Arrays.stream(ResizedPixel.values())
+            .filter(pixel -> pixel.getPixelSize() < image.getWidth())
+            .map(pixel -> resizer(fileName, fileExtension, image, pixel, s3Path)).toList();
+    }
+
+
+
+    public CustomMultipartFile resizer(String fileName, String fileExtension, BufferedImage originalImage, ResizedPixel pixel, String s3Path){
+        try {
+            int originalWidth = originalImage.getWidth();
+            int originalHeight = originalImage.getHeight();
+            int newWidth = pixel.getPixelSize();
+            int newHeight = pixel.getPixelSize() * originalHeight / originalWidth;
+            BufferedImage resizedImage = Thumbnailator.createThumbnail(originalImage, newWidth, newHeight);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ImageIO.write(resizedImage, fileExtension, out);
+            out.flush();
+            return CustomMultipartFile.builder()
+                .bytes(out.toByteArray())
+                .originalFileName(ImageUtils.createFileNameWithPixelAndExtension(fileName, fileExtension, pixel))
+                .name(ImageUtils.createResizedFilePath(s3Path, pixel, fileName, fileExtension))
+                .size(out.size())
+                .contentType(fileExtension)
+                .width(pixel.getPixelSize())
+                .build();
+        } catch (IOException ex){
+            throw new FileProcessingException(ErrorCode.FILE_PROCESSING_ERROR);
+        }
+    }
+
     private void validateFile(MultipartFile file){
+        validateFileContentType(file.getContentType());
         validateFileName(file.getOriginalFilename());
         validateFileSize(file.getSize());
+    }
+
+    private void validateFileContentType(String contentType){
+        if(!contentType.contains("image")){
+            throw new InvalidValueException(ErrorCode.INVALID_INPUT_VALUE, "파일 형식이 유효하지 않습니다.");
+        }
     }
 
     private void validateFileName(String originalFilename) {
@@ -103,39 +173,5 @@ public class AwsS3Service {
         }
     }
 
-    private String createFilePath(MultipartFile file){
-        StringBuilder sb = new StringBuilder();
-        try {
-            sb.append(s3Path).append(hashFile(file.getBytes())).append(getFileExtension(file.getOriginalFilename()));
-        } catch (IOException e) {
-            throw new FileProcessingException(ErrorCode.FILE_PROCESSING_ERROR, "파일 해싱에 실패하였습니다.");
-        }
-        return sb.toString();
-    }
 
-    private String getFileExtension(String fileName){
-        try {
-            return fileName.substring(fileName.lastIndexOf("."));
-        }catch (StringIndexOutOfBoundsException e){
-            throw new FileProcessingException(ErrorCode.FILE_PROCESSING_ERROR, "잘못된 형식의 파일(" + fileName + ")");
-        }
-    }
-
-    private String hashFile(byte[] fileData) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            byte[] hashedBytes = md.digest(fileData);
-            return bytesToHex(hashedBytes);
-        } catch (NoSuchAlgorithmException e) {
-            throw new FileProcessingException(ErrorCode.FILE_PROCESSING_ERROR, e.getMessage());
-        }
-    }
-
-    private String bytesToHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder(2 * bytes.length);
-        for (byte b : bytes) {
-            sb.append(String.format("%02x", b));
-        }
-        return sb.toString();
-    }
 }
